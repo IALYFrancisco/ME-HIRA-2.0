0a3cdbb59839fae546bb303ab7e0522d7b2f8bdd
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-09 13:34:45 +0300

    suppression du balise meta pour l'intégration des images favicon du plateforme

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,8 +5,6 @@ export default function Home() {
     <>
       <Head>
         <title>Me-Hira</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="shortcut icon" href="/images/logo-de-me-hira.png" type="image/x-icon" />
       </Head>
     </>
   );
