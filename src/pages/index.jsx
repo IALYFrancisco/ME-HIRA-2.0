cd17208830057d78cf10a384d6fe5f8b76b0a734
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-09 13:20:37 +0300

    ajout d'icon favicon sur la page principale

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,7 +6,7 @@ export default function Home() {
       <Head>
         <title>Me-Hira</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/images/logo-de-me-hira.png" type="image/x-icon" />
       </Head>
     </>
   );
