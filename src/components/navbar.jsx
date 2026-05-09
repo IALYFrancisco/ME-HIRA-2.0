8793687d2e9ed5995708fa55a03bcdfc655b65d5
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-09 13:57:35 +0300

    développement de la page d'accueil

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,18 @@
 import Image from "next/image"
+import Link from "next/link"
 
 export default function Navbar (){
     return(
         <nav>
             <ul>
                 <li>
-                    <Image src="/images/logo-de-me-hira.png" priority width={94} height={94} alt="logo de me-hira" className="logo-de-me-hira" />
+                    <Link href="/">
+                        <Image src="/images/logo-de-me-hira.png" priority width={94} height={94} alt="logo de me-hira" className="logo-de-me-hira" />
+                    </Link>
                 </li>
-                <li></li>
+                <li>
+                    <input type="text" name="song-search" id="song-search" />
+                </li>
                 <li></li>
             </ul>
         </nav>
