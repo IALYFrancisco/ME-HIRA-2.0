d4367ce348a5fcc9b3319c78c67b0231c3ed4e70
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-10 19:40:13 +0300

    ajout statiquement des vidéos dans la page principale

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -32,20 +32,20 @@ export default function Home() {
             </li>
             <li>
               <div className="poster-container">
-                <Image src="/images/Application-mobile.jpg" width={250} height={150} priority alt="poster pour les fichiers audio" className="audio-poster"/>
+                <video src="/vidéos/Antalaha_souvenir_-_Ny_Ainga(360p).mp4" controls loop></video>
               </div>
               <div className="song-info">
-                <h3>Ranomasoko</h3>
-                <h4>Rootsman</h4>
+                <h3>Antalaha souvenir</h3>
+                <h4>Ny Ainga</h4>
               </div>
             </li>
             <li>
               <div className="poster-container">
-                <Image src="/images/Application-mobile.jpg" width={250} height={150} priority alt="poster pour les fichiers audio" className="audio-poster"/>
+                <video src="/vidéos/FLORIDA_RAPHAËL_-_Jerijery___Official_Hymne_Festival_Jerijery_(1080p).mp4" controls loop></video>
               </div>
               <div className="song-info">
-                <h3>Ranomasoko</h3>
-                <h4>Rootsman</h4>
+                <h3>Jerijery___Official_Hymne_Festival_Jerijery</h3>
+                <h4>FLORIDA_RAPHAËL</h4>
               </div>
             </li>
             <li>
