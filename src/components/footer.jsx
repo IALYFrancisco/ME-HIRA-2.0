d55c9121c219d565a74bda55ccaa40656bdd0863
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-11 05:42:32 +0300

    développement du footer du plateforme

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,3 +1,21 @@
+import Link from "next/link"
+
 export default function Footer(){
-    
+    return(
+        <footer>
+            <ul>
+                <li>
+                    <Link href="">Conditions générales</Link>
+                    |<Link href="">Politiques de confidentialité</Link>|
+                    <Link href="">Mentions légales</Link>
+                </li>
+                <li>
+                    &copyright; 2026 <Link href="/">Me-Hira</Link>
+                </li>
+                <li>
+                    développée avec 💖 par <a href="https://lumini.onrender.com" target="_blank">LUMINI</a>
+                </li>
+            </ul>
+        </footer>
+    )
 }
