e50a789d3ad2bf2f5a0421ecdd1539d49d0e325a
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-11 14:13:08 +0300

    spécification selon les configurations où ajouter les fichiers sitemap et robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,6 +3,7 @@ module.exports = {
     siteUrl: process.env.NEXT_PUBLIC_APP_BASE_URL,
     generateRobotsTxt: true,
     generateIndexSitemap: false,
+    outDir: './out',
     sitemapSize: 7000,
     changefreq: 'weekly',
     priority: 1.0
