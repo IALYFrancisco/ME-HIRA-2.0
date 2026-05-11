22e63e3e24ed49f997394d70c91bd94373cbbdf6
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-11 14:12:03 +0300

    ajout de configuration pour ne générer qu'un fichier de sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,6 +2,7 @@
 module.exports = {
     siteUrl: process.env.NEXT_PUBLIC_APP_BASE_URL,
     generateRobotsTxt: true,
+    generateIndexSitemap: false,
     sitemapSize: 7000,
     changefreq: 'weekly',
     priority: 1.0
