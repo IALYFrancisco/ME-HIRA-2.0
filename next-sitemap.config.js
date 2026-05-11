0465ff15855ac0d7d0bf7f6abd113160bb8e0236
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-11 13:57:42 +0300

    configuration de génération des fichiers sitemap et robots.txt définies

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,8 @@
 /** @type { import('next-sitemap').IConfig } */
 module.exports = {
-    siteUrl: process.env.NEXT_PUBLIC_APP_BASE_URL
+    siteUrl: process.env.NEXT_PUBLIC_APP_BASE_URL,
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+    changefreq: 'weekly',
+    priority: 1.0
 }
