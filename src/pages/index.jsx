ea9e62d7748813777a8c3d01040220c67bb3bd9c
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-12 13:39:41 +0300

    dynamisation de la liste des chansons dans la page principale

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -26,114 +26,19 @@ export default function Home() {
         <Navbar/>
         <section className="songs-container">
           <ul>
-            <li>
-              <div className="poster-container">
-                <Image src="/images/17349978_5809368.jpg" width={250} height={150} priority alt="poster pour les fichiers audio" className="audio-poster"/>
-              </div>
-              <div className="song-info">
-                <h3>Ranomasoko</h3>
-                <h4>Rootsman</h4>
-              </div>
-            </li>
-            <li>
-              <div className="poster-container">
-                <Image src="/images/Application-mobile.jpg" width={250} height={150} priority alt="poster pour les fichiers audio" className="audio-poster"/>
-              </div>
-              <div className="song-info">
-                <h3>Ranomasoko</h3>
-                <h4>Rootsman</h4>
-              </div>
-            </li>
-            <li>
-              <div className="poster-container">
-                <video src="/vidéos/Antalaha_souvenir_-_Ny_Ainga(360p).mp4" controls loop></video>
-              </div>
-              <div className="song-info">
-                <h3>Antalaha souvenir</h3>
-                <h4>Ny Ainga</h4>
-              </div>
-            </li>
-            <li>
-              <div className="poster-container">
-                <video src="/vidéos/FLORIDA_RAPHAËL_-_Jerijery___Official_Hymne_Festival_Jerijery_(1080p).mp4" controls loop></video>
-              </div>
-              <div className="song-info">
-                <h3>Jerijery___Official_Hymne_Festival_Jerijery</h3>
-                <h4>FLORIDA_RAPHAËL</h4>
-              </div>
-            </li>
-            <li>
-              <div className="poster-container">
-                <Image src="/images/Application-mobile.jpg" width={250} height={150} priority alt="poster pour les fichiers audio" className="audio-poster"/>
-              </div>
-              <div className="song-info">
-                <h3>Ranomasoko</h3>
-                <h4>Rootsman</h4>
-              </div>
-            </li>
-            <li>
-              <div className="poster-container">
-                <Image src="/images/Application-mobile.jpg" width={250} height={150} priority alt="poster pour les fichiers audio" className="audio-poster"/>
-              </div>
-              <div className="song-info">
-                <h3>Ranomasoko</h3>
-                <h4>Rootsman</h4>
-              </div>
-            </li>
-            <li>
-              <div className="poster-container">
-                <Image src="/images/Application-mobile.jpg" width={250} height={150} priority alt="poster pour les fichiers audio" className="audio-poster"/>
-              </div>
-              <div className="song-info">
-                <h3>Ranomasoko</h3>
-                <h4>Rootsman</h4>
-              </div>
-            </li>
-            <li>
-              <div className="poster-container">
-                <Image src="/images/Application-mobile.jpg" width={250} height={150} priority alt="poster pour les fichiers audio" className="audio-poster"/>
-              </div>
-              <div className="song-info">
-                <h3>Ranomasoko</h3>
-                <h4>Rootsman</h4>
-              </div>
-            </li>
-            <li>
-              <div className="poster-container">
-                <Image src="/images/Application-mobile.jpg" width={250} height={150} priority alt="poster pour les fichiers audio" className="audio-poster"/>
-              </div>
-              <div className="song-info">
-                <h3>Ranomasoko</h3>
-                <h4>Rootsman</h4>
-              </div>
-            </li>
-            <li>
-              <div className="poster-container">
-                <Image src="/images/Application-mobile.jpg" width={250} height={150} priority alt="poster pour les fichiers audio" className="audio-poster"/>
-              </div>
-              <div className="song-info">
-                <h3>Ranomasoko</h3>
-                <h4>Rootsman</h4>
-              </div>
-            </li>
-            <li>
-              <div className="poster-container">
-                <Image src="/images/Application-mobile.jpg" width={250} height={150} priority alt="poster pour les fichiers audio" className="audio-poster"/>
-              </div>
-              <div className="song-info">
-                <h3>Ranomasoko</h3>
-                <h4>Rootsman</h4>
-              </div>
-            </li>
-            <li>
-              <div className="poster-container">
-                <Image src="/images/Application-mobile.jpg" width={250} height={150} priority alt="poster pour les fichiers audio" className="audio-poster"/>
-              </div>
-              <div className="song-info">
-                <h3>Ranomasoko</h3>
-                <h4>Rootsman</h4>
-              </div>
-            </li>
+            {
+              songs && songs.map((song)=>{
+                <li>
+                  <div className="poster-container">
+                    <video src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${song.fileUrl}`} controls loop></video>
+                  </div>
+                  <div className="song-info">
+                    <h3>{song.title}</h3>
+                    <h4>{song.singer}</h4>
+                  </div>
+                </li>
+              })
+            }
             <li>
               <div className="poster-container">
                 <Image src="/images/Application-mobile.jpg" width={250} height={150} priority alt="poster pour les fichiers audio" className="audio-poster"/>
