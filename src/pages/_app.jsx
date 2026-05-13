edfc52bdd418b0313ee958582e580eb1b8b5f4a9
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-13 16:49:43 +0300

    intégration du style de la page de lécture de chanson dans le projet

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import "@/styles/home.css";
+import "@/styles/songReader.css";
 import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
