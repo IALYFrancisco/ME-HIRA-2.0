25fe13d4ecc9131cdd9820dc12353452db80062f
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-13 20:00:44 +0300

    développement de la page de lecture de chanson

--- src/pages/song/[slug].jsx
+++ src/pages/song/[slug].jsx
@@ -10,6 +10,10 @@ export default function SongReader(){
                     <div className="song-poster-container">
                         <video src="http://127.0.0.1:1234/songs/FLORIDA_RAPHAËL_-_Jerijery___Official_Hymne_Festival_Jerijery_(1080p).mp4" autoplay controls loop></video>
                     </div>
+                    <div className="song-info">
+                        <h1>Jerijery</h1>
+                        <h2>FLORIDA RAPHAËL</h2>
+                    </div>
                 </div>
             </section>
             <Footer/>
