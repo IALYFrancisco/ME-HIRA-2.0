d9275ef3e9c993562197ed99966f1e77c821e887
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-14 14:00:39 +0300

    intégration encore de la page de mot de passe oublié

--- src/pages/authentication/forgotten-password.jsx
+++ src/pages/authentication/forgotten-password.jsx
@@ -4,8 +4,11 @@ export default function ForgottenPassword(){
     return(
         <>
             <Head>
-                <title>Mot de passe oublié * Me-Hira</title>
+                <title>Mot de passe oublié | Me-Hira</title>
             </Head>
+            <section className="forgotten-password-container">
+                
+            </section>
         </>
     )
 }
