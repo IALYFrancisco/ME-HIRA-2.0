f8cab57ed9b81e063348e4ab1f7e73372693c9cb
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-14 11:16:08 +0300

    intégration de la page de connexion à un compte me-hira

--- src/pages/authentication/login.jsx
+++ src/pages/authentication/login.jsx
@@ -1,5 +1,6 @@
 import Head from "next/head"
 import Navbar from "@/components/navbar"
+import Footer from "@/components/footer"
 
 export default function Login(){
     return(
@@ -8,7 +9,18 @@ export default function Login(){
                 <title>Connexion à un compte Me-Hira</title>
             </Head>
             <section className="login-page-container">
-                <Navbar/>   
+                <Navbar/>
+                <form>
+                    <div className="form-element">
+                        <label htmlFor="email">Adresse email :</label>
+                        <input type="email" name="email" id="email" placeholder="ex: name@exemple.com" required />
+                    </div>
+                    <div className="form-element">
+                        <label htmlFor="password">Mot de passe :</label>
+                        <input type="password" name="password" id="password" placeholder="choisissez un mot de passe fort" required />
+                    </div>
+                </form>
+                <Footer/>   
             </section>
         </>
     )
