643afa2813e9991ebdd3bc6d3e1b7f7c4b412c42
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-14 15:19:54 +0300

    toujours dans l'intégration de la page de réinitialisation de mot de passe

--- src/pages/authentication/reset-password.jsx
+++ src/pages/authentication/reset-password.jsx
@@ -1,13 +1,36 @@
 import Head from "next/head"
+import Navbar from "@/components/navbar"
+import Footer from "@/components/footer"
+import { useForm } from "react-hook-form"
 
 export default function ResetPassword(){
+
+    const { handleSubmit, register } = useForm()
+
+    const resetPassword = ()=>{
+
+    }
+
     return(
         <>
             <Head>
                 <title>Réinitialisation de mot de passe</title>
             </Head>
             <section className="reset-password-container">
-            
+                <Navbar/>
+                    <form onSubmit={handleSubmit(resetPassword)}>
+                        <p>Ceci est la page de réinitialisation de mot de passe, choisissez un nouveau mot de passe et confirmez-le ensuite.</p>
+                        <div className="form-element">
+                            <label htmlFor="email">Adresse email :</label>
+                            <input type="email" id="email" placeholder="ex: name@exemple.com" { ...register('email', { required: true }) } required />
+                        </div>
+                        <div className="form-element">
+                            <span className="border">
+                                <button>Envoyer le lien</button>
+                            </span>
+                        </div>
+                    </form>
+                <Footer/>
             </section>
         </>
     )
