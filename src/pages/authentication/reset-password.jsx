829bfd4542d95f684c42ab056829663790a5c308
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-14 15:15:21 +0300

    développement encore de la page de réinitialisation de mot de passe

--- src/pages/authentication/reset-password.jsx
+++ src/pages/authentication/reset-password.jsx
@@ -1,3 +1,14 @@
+import Head from "next/head"
+
 export default function ResetPassword(){
-    return()
+    return(
+        <>
+            <Head>
+                <title>Réinitialisation de mot de passe</title>
+            </Head>
+            <section className="reset-password-container">
+            
+            </section>
+        </>
+    )
 }
