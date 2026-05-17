ca91b902701c07a81aab4d6e2de45952953dc64c
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-17 10:54:35 +0300

    création de fichier composant pour le sidebar dans les pages du dashboard

--- src/components/layouts/dashboardLayout.jsx
+++ src/components/layouts/dashboardLayout.jsx
@@ -2,7 +2,6 @@
 export default function DashboardLayout({children}){
     return(
         <section className="dashboard-layout">
-            <h1>This is the dahsboard pages layout.</h1>
         </section>
     )
 }
