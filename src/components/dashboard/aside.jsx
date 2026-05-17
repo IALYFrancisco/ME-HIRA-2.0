e92d09c3e9706f0810dc633cee77731d02b053e5
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-17 13:01:16 +0300

    intégration du sidebar du dashboard

--- src/components/dashboard/aside.jsx
+++ src/components/dashboard/aside.jsx
@@ -1,7 +1,32 @@
+import Link from "next/link"
+import Image from "next/image"
+
 export default function Sidebar(){
     return(
         <aside>
-            
+            <ul>
+                <li>
+                    <Link href="/">
+                        <Image src="/images/logo-de-me-hira.png" priority width={94} height={94} alt="logo de me-hira" className="logo-de-me-hira" />
+                    </Link>
+                </li>
+                <li>
+                    <ul>
+                        <li>
+                            <Link href="">Chansons</Link>
+                        </li>
+                        <li>
+                            <Link href="">Artistes</Link>
+                        </li>
+                        <li>
+                            <Link href="">Paramètres</Link>
+                        </li>
+                        <li className="logout">
+                            <button>Se déconnecter</button>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
         </aside>
     )
 }
