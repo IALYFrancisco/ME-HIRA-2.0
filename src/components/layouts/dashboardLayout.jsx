4ffbb650e11d90425386f432cd7126dcd21d758f
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-17 09:30:31 +0300

    développement encore du layout pour les pages de dashboard

--- src/components/layouts/dashboardLayout.jsx
+++ src/components/layouts/dashboardLayout.jsx
@@ -2,7 +2,7 @@
 export default function DashboardLayout({children}){
     return(
         <section className="dashboard-layout">
-            
+            <h1>This is the dahsboard pages layout.</h1>
         </section>
     )
 }
