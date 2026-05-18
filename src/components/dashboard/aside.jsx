cf39e40dd54f048ad27fb6193b1873086263ed1f
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-18 10:06:42 +0300

    développement encore du sidebar les pages du dashboard

--- src/components/dashboard/aside.jsx
+++ src/components/dashboard/aside.jsx
@@ -5,28 +5,19 @@ export default function Sidebar(){
     return(
         <aside>
             <ul>
-                {/* <li>
-                    <Link href="/">
-                        <Image src="/images/logo-de-me-hira.png" priority width={94} height={94} alt="logo de me-hira" className="logo-de-me-hira" />
-                    </Link>
-                </li> */}
                 <li>
-                    <ul>
-                        <li>
-                            <Link href="">Chansons</Link>
-                        </li>
-                        <li>
-                            <Link href="">Artistes</Link>
-                        </li>
-                        <li>
-                            <Link href="">Paramètres</Link>
-                        </li>
-                        <li className="logout">
-                            <span>
-                                <button>Se déconnecter</button>
-                            </span>
-                        </li>
-                    </ul>
+                    <Link href="">Chansons</Link>
+                </li>
+                <li>
+                    <Link href="">Artistes</Link>
+                </li>
+                <li>
+                    <Link href="">Paramètres</Link>
+                </li>
+                <li className="logout">
+                    <span>
+                        <button>Se déconnecter</button>
+                    </span>
                 </li>
             </ul>
         </aside>
