ac80216b509faadb41a5ab0cd9f8257f0cfbd75e
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-19 14:47:55 +0300

    toujours dans le développement de la liste des chansons dans le dahsboard

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -5,10 +5,11 @@ import Head from "next/head";
 export default function Song(){
     return(
         <DashboardLayout>
-            <Head>
+            {/* <Head>
                 <title>Chansons - Me-Hira</title>
             </Head>
-            <SongsList/>
+            <SongsList/> */}
+            <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos architecto facilis error rem, sapiente totam quibusdam ducimus, voluptate sint sunt vero beatae ea possimus enim officiis consequatur nihil, placeat et.</h1>
         </DashboardLayout>
     )
 }
