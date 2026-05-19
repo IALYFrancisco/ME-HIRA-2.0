663255f2ddbc6c4c893bca5f4caedda3b210fcfe
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-19 14:25:58 +0300

    ajout de titre pour la page de liste des chansons

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,9 +1,13 @@
 import DashboardLayout from "@/components/layouts/dashboardLayout";
 import SongsList from "@/components/dashboard/songs/songs-list";
+import Head from "next/head";
 
 export default function Song(){
     return(
         <DashboardLayout>
+            <Head>
+                <title>Chansons - Me-Hira</title>
+            </Head>
             <SongsList/>
         </DashboardLayout>
     )
