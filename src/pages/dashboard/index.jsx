ea79ee4c96e538277d1e855cd14d959413be9971
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-19 14:23:23 +0300

    intégration du composant liste des chansons dans le tableau de bord

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,9 +1,10 @@
 import DashboardLayout from "@/components/layouts/dashboardLayout";
+import SongsList from "@/components/dashboard/songs/songs-list";
 
 export default function Song(){
     return(
         <DashboardLayout>
-            
+            <SongsList/>
         </DashboardLayout>
     )
 }
