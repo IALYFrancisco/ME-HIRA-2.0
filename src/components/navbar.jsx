dc42750cdf9cd92acde366d1a8f86bf20e7e4cbd
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-19 09:02:30 +0300

    ajout de l'icone de recherche dans la barre de recherche de chanson

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -13,7 +13,9 @@ export default function Navbar (){
                 <li>
                     <span className="searchbar-container">
                         <input type="text" name="song-search" id="song-search" placeholder="Rechercher des chansons ..."/>
-                        <button></button>
+                        <button>
+                            <Image src="/images/search.png" priority alt="recherche des chansons selon leur titre et chanteurs" width={48} height={48} className="search-icone" />
+                        </button>
                     </span>
                     <button className="filter">
                         <Image src="/images/filter.png" priority alt="filtre des chansons de me-hira" width={48} height={48} className="filter-icone" />
