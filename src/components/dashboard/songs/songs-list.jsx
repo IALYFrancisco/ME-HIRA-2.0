95faf5443300e0f222bdb9aa4eabc4289ef8038b
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-19 14:39:04 +0300

    développement de la page liste des chansons dans le dashboard

--- src/components/dashboard/songs/songs-list.jsx
+++ src/components/dashboard/songs/songs-list.jsx
@@ -1,3 +1,11 @@
+import Image from "next/image"
+
 export default function SongsList(){
-    return(<h1>This is the component for the songs list.</h1>)
+    return(<h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet itaque laborum fugiat, sed cum sequi commodi corrupti nulla et sunt alias expedita adipisci voluptatum aut explicabo ipsum nihil qui molestias.</h1>
+        // <section className="dashboard-songs-container">
+        //     <section className="header" style="with: 100%; height: 50px; background: red;">
+        //         <Image src="/images/logo-de-me-hira.png" priority width={94} height={94} alt="logo de me-hira" className="logo-de-me-hira" />
+        //     </section>
+        // </section>
+    )
 }
