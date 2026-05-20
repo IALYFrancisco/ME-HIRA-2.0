77d0de3389e024fcac1a5fce59f556b6343c4246
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-20 08:23:01 +0300

    développement encore de la page liste des chansons dans le dashboard

--- src/components/dashboard/songs/songs-list.jsx
+++ src/components/dashboard/songs/songs-list.jsx
@@ -4,8 +4,18 @@ export default function SongsList(){
     return(
         <section className="dashboard-songs-container">
             <section className="header">
-                <Image src="/images/logo-de-me-hira.png" priority width={94} height={94} alt="logo de me-hira" className="logo-de-me-hira" />
-                <h1>Chansons</h1>
+                <div className="page-title">
+                    <Image src="/images/logo-de-me-hira.png" priority width={94} height={94} alt="logo de me-hira" className="logo-de-me-hira" />
+                    <h1>Chansons</h1>
+                </div>
+                <div className="actions-container">
+                    <div className="filters-container">
+                        <input type="text" name="" id="" placeholder="Rechercher des chansons ..." />
+                    </div>
+                    <span>
+                        <button>Ajouter une chanson</button>
+                    </span>
+                </div>
             </section>
         </section>
     )
