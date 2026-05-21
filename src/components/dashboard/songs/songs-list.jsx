a6b1b0dc37238a92ff676985436c1d9809110f43
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-21 10:02:07 +0300

    toujours dans le développement de la page de chanson dans le dahsboard

--- src/components/dashboard/songs/songs-list.jsx
+++ src/components/dashboard/songs/songs-list.jsx
@@ -14,20 +14,22 @@ export default function SongsList(){
 
     return(
         <section className="dashboard-songs-container">
-            <section className="header">
-                <div className="page-title">
-                    <Image src="/images/logo-de-me-hira.png" priority width={94} height={94} alt="logo de me-hira" className="logo-de-me-hira" />
-                    <h1>Chansons</h1>
-                </div>
-                <div className="actions-container">
-                    <div className="filters-container">
-                        <input type="text" name="" id="" placeholder="Rechercher des chansons ..." />
+            <div className="page-header-container">
+                <section className="header">
+                    <div className="page-title">
+                        <Image src="/images/logo-de-me-hira.png" priority width={94} height={94} alt="logo de me-hira" className="logo-de-me-hira" />
+                        <h1>Chansons</h1>
                     </div>
-                    <span>
-                        <button>Ajouter une chanson</button>
-                    </span>
-                </div>
-            </section>
+                    <div className="actions-container">
+                        <div className="filters-container">
+                            <input type="text" name="" id="" placeholder="Rechercher des chansons ..." />
+                        </div>
+                        <span>
+                            <button>Ajouter une chanson</button>
+                        </span>
+                    </div>
+                </section>
+            </div>
             <section className="body">
                 <table>
                     <thead>
