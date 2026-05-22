270667ca95fb52ac25b62162a6c7f7fefe5f8f6d
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-22 08:20:14 +0300

    ajout de l'overlay pour le formulaire d'ajout de chanson

--- src/components/dashboard/songs/songs-list.jsx
+++ src/components/dashboard/songs/songs-list.jsx
@@ -13,54 +13,56 @@ export default function SongsList(){
     }, [])
 
     return(
-        <section className="dashboard-songs-container">
-            <div className="page-header-container">
-                <section className="header">
-                    <div className="page-title">
-                        <Image src="/images/logo-de-me-hira.png" priority width={94} height={94} alt="logo de me-hira" className="logo-de-me-hira" />
-                        <h1>Chansons</h1>
-                    </div>
-                    <div className="actions-container">
-                        <div className="filters-container">
-                            <input type="text" name="" id="" placeholder="Rechercher des chansons ..." />
+        <>
+            <section className="dashboard-songs-container">
+                <div className="page-header-container">
+                    <section className="header">
+                        <div className="page-title">
+                            <Image src="/images/logo-de-me-hira.png" priority width={94} height={94} alt="logo de me-hira" className="logo-de-me-hira" />
+                            <h1>Chansons</h1>
                         </div>
-                        <span>
-                            <button>Ajouter une chanson</button>
-                        </span>
-                    </div>
+                        <div className="actions-container">
+                            <div className="filters-container">
+                                <input type="text" name="" id="" placeholder="Rechercher des chansons ..." />
+                            </div>
+                            <span>
+                                <button>Ajouter une chanson</button>
+                            </span>
+                        </div>
+                    </section>
+                </div>
+                <section className="body">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Titre</th>
+                                <th>Chanteur</th>
+                                <th>Auteur</th>
+                                <th>Compositeur</th>
+                                <th>Publiée ?</th>
+                                <th className="actions">Actions</th>
+                            </tr>
+                        </thead>
+                        { songs &&
+                            <tbody>
+                                { songs.map(song=>(
+                                    <tr key={song._id}>
+                                        <td>{song.title}</td>
+                                        <td>{song.singer}</td>
+                                        <td>{song.author ? song.author : "------------"}</td>
+                                        <td>{song.composer ? song.composer : "------------"}</td>
+                                        <td>
+                                            <span className={song.published ? "song-badge yes" : "song-badge no"}>{song.published ? "Oui" : "Non"}</span>
+                                        </td>
+                                        <td className="actions">Actions</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        }
+                    </table>
                 </section>
-            </div>
-            <section className="body">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Titre</th>
-                            <th>Chanteur</th>
-                            <th>Auteur</th>
-                            <th>Compositeur</th>
-                            <th>Publiée ?</th>
-                            <th className="actions">Actions</th>
-                        </tr>
-                    </thead>
-                    { songs &&
-                        <tbody>
-                            { songs.map(song=>(
-                                <tr key={song._id}>
-                                    <td>{song.title}</td>
-                                    <td>{song.singer}</td>
-                                    <td>{song.author ? song.author : "------------"}</td>
-                                    <td>{song.composer ? song.composer : "------------"}</td>
-                                    <td>
-                                        <span className={song.published ? "song-badge yes" : "song-badge no"}>{song.published ? "Oui" : "Non"}</span>
-                                    </td>
-                                    <td className="actions">Actions</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    }
-                </table>
             </section>
-        </section>
-        <div className="add-song-overlay"></div>
+            <div className="add-song-overlay"></div>
+        </>
     )
 }
