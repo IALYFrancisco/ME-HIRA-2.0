11e466b13640e6393e3f38db5bcb5d6cf64048c0
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-22 21:26:11 +0300

    développement encore du modal pour la barre de recherche de chanson dans la page principale

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,24 @@
 import Image from "next/image"
 import Link from "next/link"
+import { useEffect, useRef } from "react"
+import { useForm } from "react-hook-form"
 
 export default function Navbar (){
+
+    const { register, watch } = useForm()
+
+    const watchAll = watch()
+
+    var searchSongModalRef = useRef(null)
+
+    useEffect(()=>{
+        if(watchAll.songSearch){
+            searchSongModalRef.current.classList.add('active')
+        }else{
+            searchSongModalRef.current.classList.remove('active')
+        }
+    },[watchAll])
+
     return(
         <nav>
             <ul>
@@ -12,8 +29,8 @@ export default function Navbar (){
                 </li>
                 <li>
                     <span className="searchbar-container">
-                        <input type="text" name="song-search" id="song-search" placeholder="Rechercher des chansons ..."/>
-                        <div className="home-search-modal"></div>
+                        <input type="text" id="songSearch" placeholder="Rechercher des chansons ..." {...register('songSearch')}/>
+                        <div className="home-search-modal" ref={searchSongModalRef}></div>
                         <button>
                             <Image src="/images/search.png" priority alt="recherche des chansons selon leur titre et chanteurs" width={48} height={48} className="search-icone" />
                         </button>
