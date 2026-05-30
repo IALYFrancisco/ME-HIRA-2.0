0268be5cf6d4d0d11cc98b0f4f779a7d4921a63d
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-30 10:37:14 +0300

    intercepteur des réponses et requêtes http développé

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -4,3 +4,18 @@ const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
     withCredentials: true
 })
+
+api.interceptors.response.use(
+    response => response,
+    async error => {
+        const originalRequest = error.config
+        if(error.response.status == 401 && !originalRequest._retry){
+            originalRequest._retry = true
+            const response = await api.post("/authentication/refresh-token")
+            const accessToken = response.data.accessToken
+            originalRequest.headers.Authorization = `Bearer ${accessToken}`
+            return api(originalRequest)
+        }
+        return Promise.reject(error)
+    }
+)
