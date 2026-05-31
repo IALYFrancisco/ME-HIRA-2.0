69abe0bc4aefb6cc87c1cd13944ccc74fddc1d47
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-31 19:02:03 +0300

    petite amélioration du helper API

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -4,3 +4,11 @@ export const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
     withCredentials: true
 })
+
+api.interceptors.request.use((config)=>{
+    const tk_sid = localStorage.getItem("tk.sid")
+    if(token){
+        config.headers.Authorization = `Bearer ${tk_sid}`
+    }
+    return config
+})
