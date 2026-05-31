bd414772f098aa687ff63d31f807387aeb9a847f
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-31 12:55:00 +0300

    développement encore du contexte d'authentification

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,2 +1,14 @@
 import { createContext, useState, useEffect, useContext } from "react"
 import { api } from "@/helpers/api"
+
+const AuthContext = createContext()
+
+export const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+    useEffect(()=>{
+        api.get('/user/informations')
+            .then(response=>setUser(response.data))
+            .finally(()=>setLoading(false))
+    }, [])
+}
