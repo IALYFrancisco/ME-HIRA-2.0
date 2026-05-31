019f9c9e8a28e1dd88350666deef2c7d5562bfc1
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-31 13:12:19 +0300

    développement encore du contexte d'authentification

--- src/components/isNotAuthenticated.jsx
+++ src/components/isNotAuthenticated.jsx
@@ -3,3 +3,14 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { useAuth } from "@/contexts/AuthContext";
+
+export default function IsNotAuthenticated({children}){
+    const router = useRouter()
+    const { user, loading } = useAuth()
+    useEffect(()=>{
+        if(!loading && user){
+            router.replace("/dashboard")
+        }
+    }, [user, loading, router])
+    return children
+}
