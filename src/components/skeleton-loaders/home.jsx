4a4382385bc8bc6f709fef20eb29a47f448f72d5
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-06-01 22:58:55 +0300

    toujours dans le développement du chargement de la page principale

--- src/components/skeleton-loaders/home.jsx
+++ src/components/skeleton-loaders/home.jsx
@@ -6,52 +6,11 @@ export default function HomeSkeletonLoader(){
             <ul>
                 <li>
                     <div className="poster-container">
-                        <Image src="/images/Application-mobile.jpg" width={250} height={150} priority alt="poster pour les fichiers audio" className="audio-poster"/>
                     </div>
                     <div className="song-info">
                         <h3>Titre du chanson</h3>
                         <h4>Chanteur(s)</h4>
-                        <p><span className="badge">video</span>13 déc 2026</p>
-                    </div>
-                </li>
-                <li>
-                    <div className="poster-container">
-                        <Image src="/images/Application-mobile.jpg" width={250} height={150} priority alt="poster pour les fichiers audio" className="audio-poster"/>
-                    </div>
-                    <div className="song-info">
-                        <h3>Titre du chanson</h3>
-                        <h4>Chanteur(s)</h4>
-                        <p><span className="badge">video</span>13 déc 2026</p>
-                    </div>
-                </li>
-                <li>
-                    <div className="poster-container">
-                        <Image src="/images/Application-mobile.jpg" width={250} height={150} priority alt="poster pour les fichiers audio" className="audio-poster"/>
-                    </div>
-                    <div className="song-info">
-                        <h3>Titre du chanson</h3>
-                        <h4>Chanteur(s)</h4>
-                        <p><span className="badge">video</span>13 déc 2026</p>
-                    </div>
-                </li>
-                <li>
-                    <div className="poster-container">
-                        <Image src="/images/Application-mobile.jpg" width={250} height={150} priority alt="poster pour les fichiers audio" className="audio-poster"/>
-                    </div>
-                    <div className="song-info">
-                        <h3>Titre du chanson</h3>
-                        <h4>Chanteur(s)</h4>
-                        <p><span className="badge">video</span>13 déc 2026</p>
-                    </div>
-                </li>
-                <li>
-                    <div className="poster-container">
-                        <Image src="/images/Application-mobile.jpg" width={250} height={150} priority alt="poster pour les fichiers audio" className="audio-poster"/>
-                    </div>
-                    <div className="song-info">
-                        <h3>Titre du chanson</h3>
-                        <h4>Chanteur(s)</h4>
-                        <p><span className="badge">video</span>13 déc 2026</p>
+                        <p><span className="skeleton-badge">video</span>13 déc 2026</p>
                     </div>
                 </li>
             </ul>
