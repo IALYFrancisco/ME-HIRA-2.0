3ac6a05abe9f6d5d470b768edcd6fcf3a282d993
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-06-01 23:05:07 +0300

    développement encore du chargement de la page principale

--- src/components/skeleton-loaders/home.jsx
+++ src/components/skeleton-loaders/home.jsx
@@ -11,6 +11,87 @@ export default function HomeSkeletonLoader(){
                         <p><span className="skeleton-badge">video</span>13 déc 2026</p>
                     </div>
                 </li>
+                <li>
+                    <div className="poster-container">
+                    </div>
+                    <div className="song-info">
+                        <h3>Titre du chanson</h3>
+                        <h4>Chanteur(s)</h4>
+                        <p><span className="skeleton-badge">video</span>13 déc 2026</p>
+                    </div>
+                </li>
+                <li>
+                    <div className="poster-container">
+                    </div>
+                    <div className="song-info">
+                        <h3>Titre du chanson</h3>
+                        <h4>Chanteur(s)</h4>
+                        <p><span className="skeleton-badge">video</span>13 déc 2026</p>
+                    </div>
+                </li>
+                <li>
+                    <div className="poster-container">
+                    </div>
+                    <div className="song-info">
+                        <h3>Titre du chanson</h3>
+                        <h4>Chanteur(s)</h4>
+                        <p><span className="skeleton-badge">video</span>13 déc 2026</p>
+                    </div>
+                </li>
+                <li>
+                    <div className="poster-container">
+                    </div>
+                    <div className="song-info">
+                        <h3>Titre du chanson</h3>
+                        <h4>Chanteur(s)</h4>
+                        <p><span className="skeleton-badge">video</span>13 déc 2026</p>
+                    </div>
+                </li>
+                <li>
+                    <div className="poster-container">
+                    </div>
+                    <div className="song-info">
+                        <h3>Titre du chanson</h3>
+                        <h4>Chanteur(s)</h4>
+                        <p><span className="skeleton-badge">video</span>13 déc 2026</p>
+                    </div>
+                </li>
+                <li>
+                    <div className="poster-container">
+                    </div>
+                    <div className="song-info">
+                        <h3>Titre du chanson</h3>
+                        <h4>Chanteur(s)</h4>
+                        <p><span className="skeleton-badge">video</span>13 déc 2026</p>
+                    </div>
+                </li>
+                <li>
+                    <div className="poster-container">
+                    </div>
+                    <div className="song-info">
+                        <h3>Titre du chanson</h3>
+                        <h4>Chanteur(s)</h4>
+                        <p><span className="skeleton-badge">video</span>13 déc 2026</p>
+                    </div>
+                </li>
+                <li>
+                    <div className="poster-container">
+                    </div>
+                    <div className="song-info">
+                        <h3>Titre du chanson</h3>
+                        <h4>Chanteur(s)</h4>
+                        <p><span className="skeleton-badge">video</span>13 déc 2026</p>
+                    </div>
+                </li>
+                <li>
+                    <div className="poster-container">
+                    </div>
+                    <div className="song-info">
+                        <h3>Titre du chanson</h3>
+                        <h4>Chanteur(s)</h4>
+                        <p><span className="skeleton-badge">video</span>13 déc 2026</p>
+                    </div>
+                </li>
             </ul>
         </section>
     )
