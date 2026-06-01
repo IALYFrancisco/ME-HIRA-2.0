3df95c69967344fa95d0d27b028f868692e26ee1
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-06-01 22:18:11 +0300

    développement encore du skeleton chargement de la page principale

--- src/components/skeleton-loaders/home.jsx
+++ src/components/skeleton-loaders/home.jsx
@@ -1,28 +1,20 @@
+import Image from "next/image"
+
 export default function HomeSkeletonLoader(){
     return(
         <section className="songs-container">
-                  <ul>
-                    {
-                      songs && songs.map((song)=>
-                          <li key={song._id}>
-                            <Link href={`/song/${song.slug}`}>
-                              <div className="poster-container">
-                                { 
-                                  song.fileType === "video" ?
-                                  <video src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${song.fileUrl}`} loop></video> :
-                                  <Image src="/images/Application-mobile.jpg" width={250} height={150} priority alt="poster pour les fichiers audio" className="audio-poster"/>
-                                }
-                              </div>
-                              <div className="song-info">
-                                <h3>{song.title}</h3>
-                                <h4>{song.singer}</h4>
-                                <p><span className="badge">{ song.fileType === 'video' ? 'vidéo' : 'audio'}</span>{formatDateMG(song.updatedAt)}</p>
-                              </div>
-                            </Link>
-                          </li>
-                      )
-                    }
-                  </ul>
-                </section>
+            <ul>
+                <li>
+                    <div className="poster-container">
+                        <Image src="/images/Application-mobile.jpg" width={250} height={150} priority alt="poster pour les fichiers audio" className="audio-poster"/>
+                    </div>
+                    <div className="song-info">
+                        <h3>Titre du chanson</h3>
+                        <h4>Chanteur(s)</h4>
+                        <p><span className="badge">video</span>13 déc 2026</p>
+                    </div>
+                </li>
+            </ul>
+        </section>
     )
 }
