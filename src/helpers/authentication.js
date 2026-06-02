920f63486d93128c0be4d18e54b4a382e6c46ae7
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-06-02 20:52:20 +0300

    logique de rafraîchissement de token définie

--- src/helpers/authentication.js
+++ src/helpers/authentication.js
@@ -1,3 +1,20 @@
-export function RefreshToken(){
-    
+import { useAuth } from "@/contexts/AuthContext";
+import { api } from "./api";
+
+export function RefreshToken(request){
+    const { setUser } = useAuth()
+    api.post('/authentication/refresh-token')
+    .then((response)=>{
+        if(response.status === 209){
+            api.post('/authentication/logout')
+            .then(()=>{
+                setUser(null)
+                localStorage.removeItem("at.sid")
+                return 
+            })
+        }
+        if(response.status === 200){
+            request
+        }
+    })
 }
