d1af303bdccac24462c811d7c2e15db7897f9832
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-06-02 16:06:34 +0300

    développement encore du chargement de liste des chansons dans le dashboard

--- src/components/skeleton-loaders/songsListSkeleton.jsx
+++ src/components/skeleton-loaders/songsListSkeleton.jsx
@@ -11,6 +11,76 @@ export default function SongsListSkeleton(){
                 </td>
                 <td className="actions">Actions</td>
             </tr>
+            <tr>
+                <td>Titre du chanson</td>
+                <td>Chanteur(s)/euses</td>
+                <td>Auteur(s)</td>
+                <td>Composer(s)</td>
+                <td>
+                    <span className="song-badge"></span>
+                </td>
+                <td className="actions">Actions</td>
+            </tr>
+            <tr>
+                <td>Titre du chanson</td>
+                <td>Chanteur(s)/euses</td>
+                <td>Auteur(s)</td>
+                <td>Composer(s)</td>
+                <td>
+                    <span className="song-badge"></span>
+                </td>
+                <td className="actions">Actions</td>
+            </tr>
+            <tr>
+                <td>Titre du chanson</td>
+                <td>Chanteur(s)/euses</td>
+                <td>Auteur(s)</td>
+                <td>Composer(s)</td>
+                <td>
+                    <span className="song-badge"></span>
+                </td>
+                <td className="actions">Actions</td>
+            </tr>
+            <tr>
+                <td>Titre du chanson</td>
+                <td>Chanteur(s)/euses</td>
+                <td>Auteur(s)</td>
+                <td>Composer(s)</td>
+                <td>
+                    <span className="song-badge"></span>
+                </td>
+                <td className="actions">Actions</td>
+            </tr>
+            <tr>
+                <td>Titre du chanson</td>
+                <td>Chanteur(s)/euses</td>
+                <td>Auteur(s)</td>
+                <td>Composer(s)</td>
+                <td>
+                    <span className="song-badge"></span>
+                </td>
+                <td className="actions">Actions</td>
+            </tr>
+            <tr>
+                <td>Titre du chanson</td>
+                <td>Chanteur(s)/euses</td>
+                <td>Auteur(s)</td>
+                <td>Composer(s)</td>
+                <td>
+                    <span className="song-badge"></span>
+                </td>
+                <td className="actions">Actions</td>
+            </tr>
+            <tr>
+                <td>Titre du chanson</td>
+                <td>Chanteur(s)/euses</td>
+                <td>Auteur(s)</td>
+                <td>Composer(s)</td>
+                <td>
+                    <span className="song-badge"></span>
+                </td>
+                <td className="actions">Actions</td>
+            </tr>
         </tbody>
     )
 }
