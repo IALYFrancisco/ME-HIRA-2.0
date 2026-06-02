7d173350193aa43dfefd2a60c55e683f076fa9dc
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-06-02 16:02:03 +0300

    ajout de contenus dans le composant SongsListSkeleton

--- src/components/skeleton-loaders/songsListSkeleton.jsx
+++ src/components/skeleton-loaders/songsListSkeleton.jsx
@@ -1,3 +1,16 @@
 export default function SongsListSkeleton(){
-    return
+    return(
+        <tbody>
+            <tr>
+                <td>Titre du chanson</td>
+                <td>Chanteur(s)/euses</td>
+                <td>Auteur(s)</td>
+                <td>Composer(s)</td>
+                <td>
+                    <span className="song-badge"></span>
+                </td>
+                <td className="actions">Actions</td>
+            </tr>
+        </tbody>
+    )
 }
