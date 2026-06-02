5399d6e08e4c9ccc89a6924e07f5eed89e06fd4d
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-06-02 04:40:14 +0300

    développement du chargement de la page de lécture de chanson

--- src/components/skeleton-loaders/songReader.jsx
+++ src/components/skeleton-loaders/songReader.jsx
@@ -3,12 +3,11 @@ export default function SongReaderSkeletonLoader(){
         <section className="song-container">
             <div className="song">
                 <div className="song-poster-container">
-                    <video src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${song.fileUrl}`} autoPlay controls loop></video>
                 </div>
                 <div className="song-info">
-                    <h1>{song.title}</h1>
-                    <h2>{song.singer}</h2>
-                    <p><span className="badge">{song.fileType}</span>{formatDateMG(song.updatedAt)}</p>
+                    <h1>Titre du chanson</h1>
+                    <h2>Chanteur(s)</h2>
+                    <p><span className="badge">vidéo</span>13 mai 2026</p>
                 </div>
             </div>
         </section>
