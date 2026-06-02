0e21894e7a4ea364bc771882340fdf5ed8f38dba
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-06-02 04:36:02 +0300

    intégration du style de chargement de la page de lécture de chanson

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -6,6 +6,7 @@ import "@/styles/authentication/login.css";
 import "@/styles/authentication/resetPassword.css";
 import "@/styles/authentication/forgottenPassword.css";
 import "@/styles/skeleton-loaders/home.css"
+import "@/styles/skeleton-loaders/songReaderSkeleton.css"
 import Head from "next/head";
 import { Toaster } from "sonner"
 import { AuthProvider } from "@/contexts/AuthContext";
