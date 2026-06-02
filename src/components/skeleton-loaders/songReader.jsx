7e2bb756261f341c271a79cf7dd4ea256f11b0c8
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-06-02 04:37:43 +0300

    ajout de contenu dans le compsoant de changrement du page de lécture de chanson

--- src/components/skeleton-loaders/songReader.jsx
+++ src/components/skeleton-loaders/songReader.jsx
@@ -1,5 +1,16 @@
 export default function SongReaderSkeletonLoader(){
     return(
-        
+        <section className="song-container">
+            <div className="song">
+                <div className="song-poster-container">
+                    <video src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${song.fileUrl}`} autoPlay controls loop></video>
+                </div>
+                <div className="song-info">
+                    <h1>{song.title}</h1>
+                    <h2>{song.singer}</h2>
+                    <p><span className="badge">{song.fileType}</span>{formatDateMG(song.updatedAt)}</p>
+                </div>
+            </div>
+        </section>
     )
 }
