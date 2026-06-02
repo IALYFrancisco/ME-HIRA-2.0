9bcb688bc327cd411c710fb6a0094cf051c5c6d9
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-06-02 09:10:47 +0300

    amélioration de l'état de chargement du formulaire de réinitialisation de mot de passe

--- src/pages/authentication/reset-password.jsx
+++ src/pages/authentication/reset-password.jsx
@@ -4,11 +4,13 @@ import Footer from "@/components/footer"
 import { useForm } from "react-hook-form"
 import IsNotAuthenticated from "@/components/isNotAuthenticated"
 import { useAuth } from "@/contexts/AuthContext"
+import { useState } from "react"
 
 export default function ResetPassword(){
 
     const { handleSubmit, register } = useForm()
     const { loading } = useAuth()
+    var [resetPasswordLoading, setResetPasswordLoading] = useState(false)
 
     const resetPassword = ()=>{
 
@@ -25,15 +27,15 @@ export default function ResetPassword(){
                         <p>Ceci est la page de réinitialisation de mot de passe, choisissez un nouveau mot de passe et confirmez-le ensuite.</p>
                         <div className="form-element">
                             <label htmlFor="newPassword">Nouveau mot de passe :</label>
-                            <input type="password" id="newPassword" placeholder="Choisissez un mot de passe fort" { ...register('newPassword', { required: true }) } required disabled={loading}/>
+                            <input type="password" id="newPassword" placeholder="Choisissez un mot de passe fort" { ...register('newPassword', { required: true }) } required disabled={loading || resetPasswordLoading}/>
                         </div>
                         <div className="form-element">
                             <label htmlFor="password">Confirmez le nouveau mot de passe :</label>
-                            <input type="password" id="password" placeholder="Le même mot de passe que le dessus" { ...register('password', { required: true }) } required disabled={loading}/>
+                            <input type="password" id="password" placeholder="Le même mot de passe que le dessus" { ...register('password', { required: true }) } required disabled={loading || resetPasswordLoading}/>
                         </div>
                         <div className="form-element">
-                            <span className={loading?"border disabled":"border"}>
-                                <button disabled={loading}>Soumettre</button>
+                            <span className={(loading || resetPasswordLoading)?"border disabled":"border"}>
+                                <button disabled={loading || resetPasswordLoading}>Soumettre</button>
                             </span>
                         </div>
                     </form>
