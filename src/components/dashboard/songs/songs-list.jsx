ba5be87d27676ec562b72eb1121a67a157a59bf6
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-06-17 19:46:51 +0300

    ajout de logique de click sur les icones des actions

--- src/components/dashboard/songs/songs-list.jsx
+++ src/components/dashboard/songs/songs-list.jsx
@@ -159,7 +159,7 @@ export default function SongsList(){
                                                 <li>Modifier</li>
                                                 <li>Supprimer</li>
                                             </ul>
-                                            <Image ref={popUpActionsRef} src="/images/song-menu-actions.png" width={16} height={16} priority alt="menu des actions sur chaque chanson"/>
+                                            <Image onClick={()=>toggleActionsPopUp(song._id)} ref={popUpActionsRef} src="/images/song-menu-actions.png" width={16} height={16} priority alt="menu des actions sur chaque chanson"/>
                                         </td>
                                     </tr>
                                 ))}
