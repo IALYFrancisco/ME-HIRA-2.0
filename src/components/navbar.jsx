f6e392680a33b1927ede172d8a18cfb56cf88f19
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-06-17 08:14:34 +0300

    développement de l'interface de filtre par type des chansons

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -37,46 +37,48 @@ export default function Navbar (){
     },[prompt])
 
     return(
-        <nav>
-            <ul>
-                <li>
-                    <Link href="/">
-                        <Image src="/images/logo-de-me-hira.png" priority width={94} height={94} alt="logo de me-hira" className="logo-de-me-hira" />
-                    </Link>
-                </li>
-                <li>
-                    <span className="searchbar-container">
-                        <input type="text" id="songSearch" placeholder="Rechercher des chansons ..." value={prompt} onChange={(e)=>{setPrompt(e.target.value)}}/>
-                        <div className={((prompt && results.length > 0)) ? "home-search-modal active" : "home-search-modal"}>
-                            <ul>
-                                { results.map((song)=>(
-                                    <li key={song._id}>
-                                        <Link href={`/song/${song.slug}`}>
-                                            <h4>{song.title}</h4>
-                                            <span className="singer-container">
-                                                <h5>{FormatSongSinger(song.singer)}</h5>
-                                                <span className="badge">{song.fileType}</span>
-                                            </span>
-                                        </Link>
-                                    </li>
-                                )) }
-                            </ul>
-                        </div>
-                        <button>
-                            <Image src="/images/search.png" priority alt="recherche des chansons selon leur titre et chanteurs" width={48} height={48} className="search-icone" />
+        <>
+            <nav>
+                <ul>
+                    <li>
+                        <Link href="/">
+                            <Image src="/images/logo-de-me-hira.png" priority width={94} height={94} alt="logo de me-hira" className="logo-de-me-hira" />
+                        </Link>
+                    </li>
+                    <li>
+                        <span className="searchbar-container">
+                            <input type="text" id="songSearch" placeholder="Rechercher des chansons ..." value={prompt} onChange={(e)=>{setPrompt(e.target.value)}}/>
+                            <div className={((prompt && results.length > 0)) ? "home-search-modal active" : "home-search-modal"}>
+                                <ul>
+                                    { results.map((song)=>(
+                                        <li key={song._id}>
+                                            <Link href={`/song/${song.slug}`}>
+                                                <h4>{song.title}</h4>
+                                                <span className="singer-container">
+                                                    <h5>{FormatSongSinger(song.singer)}</h5>
+                                                    <span className="badge">{song.fileType}</span>
+                                                </span>
+                                            </Link>
+                                        </li>
+                                    )) }
+                                </ul>
+                            </div>
+                            <button>
+                                <Image src="/images/search.png" priority alt="recherche des chansons selon leur titre et chanteurs" width={48} height={48} className="search-icone" />
+                            </button>
+                        </span>
+                        <button className="filter">
+                            <Image src="/images/filter.png" priority alt="filtre des chansons de me-hira" width={48} height={48} className="filter-icone" />
                         </button>
-                    </span>
-                    <button className="filter">
-                        <Image src="/images/filter.png" priority alt="filtre des chansons de me-hira" width={48} height={48} className="filter-icone" />
-                    </button>
-                </li>
-                <li>
-                    <Link href="/authentication/login">
-                        <button>Se connecter</button>
-                    </Link>
-                </li>
-            </ul>
-            <div className="filter-type-overlay"></div>
-        </nav>
+                    </li>
+                    <li>
+                        <Link href="/authentication/login">
+                            <button>Se connecter</button>
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+            <div className="filter-type-overlay active"></div>
+        </>
     )
 }
