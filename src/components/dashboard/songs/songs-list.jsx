2965f1dfb4c7402063db020ad7e27aac5e38b4fe
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-06-18 07:45:08 +0300

    petite modification du fonction fetchSong

--- src/components/dashboard/songs/songs-list.jsx
+++ src/components/dashboard/songs/songs-list.jsx
@@ -41,13 +41,9 @@ export default function SongsList(){
     const searchSongs = async (p) => api.get(`/song/get?prompt=${p}`)
 
     const fetchSongs = async (value)=>{
-        try {
-            const response = await searchSongs(value)
-            let _song = response.data?.filter(s=>s.published===true)
-            setResults(_song)
-        }catch{
-            
-        }
+        const response = await searchSongs(value)
+        let _song = response.data?.filter(s=>s.published===true)
+        setResults(_song)
     }
 
     const toggleActionsPopUp = (songId) => {
