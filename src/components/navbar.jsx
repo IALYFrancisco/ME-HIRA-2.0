db78c4c1fb24e1c094c817124a155b140bd1e106
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-06-21 10:48:15 +0300

    intégration dans la barre de recherche du loader de recherche

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -77,7 +77,8 @@ export default function Navbar (){
                             </ul>
                         </div>
                         <button>
-                            <Image src="/images/search.png" priority alt="recherche des chansons selon leur titre et chanteurs" width={48} height={48} className="search-icone" />
+                            {/* <Image src="/images/search.png" priority alt="recherche des chansons selon leur titre et chanteurs" width={48} height={48} className="search-icone" /> */}
+                            <Image src="/images/spinner (1).svg" priority alt="recherche des chansons selon leur titre et chanteurs" width={48} height={48} className="search-icone" />
                         </button>
                     </span>
                     <div className="filter-type-container">
