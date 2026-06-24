80eab79836d7381f2515c84193e18063b3f7b528
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-06-24 14:48:32 +0300

    développement encore du fonction de modification de chanson

--- src/components/dashboard/songs/songs-list.jsx
+++ src/components/dashboard/songs/songs-list.jsx
@@ -224,16 +224,21 @@ export default function SongsList(){
 
     const updateSong = async (song) => {
         try{
+            
             setSongActionIsLoading(true)
-            let response = await api.patch('/song/update', { song: song._id, update: song?.published ? { published: false } : { published: true }})
-            if(response.status === 200){
-                toast.info(`La chanson intitulée ${song?.title} est actuellement ${song?.published ? 'indisponible' : 'disponible'} en publique.`)
-                api.get('/song/get')
-                    .then((response) => {
-                        setSongs(response.data)
-                    })
-                    .catch(()=>toast.error("Erreur de récupération de la nouvelle liste des chansons."))
-            }
+            setUpdatingSongFormIsActive(true)
+            reset({})
+            openAddSongModal()
+
+            // let response = await api.patch('/song/update', { song: song._id, update: song?.published ? { published: false } : { published: true }})
+            // if(response.status === 200){
+            //     toast.info(`La chanson intitulée ${song?.title} est actuellement ${song?.published ? 'indisponible' : 'disponible'} en publique.`)
+            //     api.get('/song/get')
+            //         .then((response) => {
+            //             setSongs(response.data)
+            //         })
+            //         .catch(()=>toast.error("Erreur de récupération de la nouvelle liste des chansons."))
+            // }
         }catch{
             toast.error("Erreur de mise à jour du chanson, veuillez réessayer plus tard.")
         }finally{
