9bef9263492b02f466be0f654e01a32ae8f3141b
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-03 15:56:52 +0300

    développement de l'en-tête de page pour les paramètres

--- src/pages/dashboard/settings.jsx
+++ src/pages/dashboard/settings.jsx
@@ -1,5 +1,6 @@
 import DashboardLayout from "@/components/layouts/dashboardLayout";
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Settings(){
     return(
@@ -9,7 +10,15 @@ export default function Settings(){
                     Paramètres - Me-Hira
                 </title>
             </Head>
-            <section className="dashboard-settings-container"></section>
+            <section className="dashboard-settings-container">
+                <div className="page-header-container">
+                    <section className="header">
+                        <div className="page-title">
+                            <Link></Link>
+                        </div>
+                    </section>
+                </div>
+            </section>
         </DashboardLayout>
     )
 }
