1462fc8325dccb89547b2a10b9dfb13d32a42418
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-03 15:48:23 +0300

    toujours dans le développement du paramètre

--- src/pages/dashboard/settings.jsx
+++ src/pages/dashboard/settings.jsx
@@ -1,9 +1,14 @@
 import DashboardLayout from "@/components/layouts/dashboardLayout";
+import Head from "next/head";
 
 export default function Settings(){
     return(
         <DashboardLayout>
-            
+            <Head>
+                <title>
+                    Paramètres - Me-Hira
+                </title>
+            </Head>
         </DashboardLayout>
     )
 }
