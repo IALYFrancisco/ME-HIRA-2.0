0c5e7deb42b69b0b0740fdcde1d628a0e207c1c6
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-04 14:44:55 +0300

    toujours dans le développement du formulaire de changement d'informations personnelles

--- src/components/dashboard/settings/personalInfosEditingForm.jsx
+++ src/components/dashboard/settings/personalInfosEditingForm.jsx
@@ -1,19 +1,29 @@
 import { useForm } from "react-hook-form";
+import { useAuth } from "@/contexts/AuthContext";
+import { useEffect } from "react";
 
 export default function PersonalInfosEditingForm(){
 
-    const { register } = useForm()
+    const { register, reset } = useForm()
+    const { user } = useAuth()
+
+    useEffect(()=>{
+        reset({
+            name: user.name,
+            email: user.email
+        })
+    },[user])
 
     return(
         <form>
             <h2>Informations personnelles :</h2>
             <div className="form-element">
                 <label htmlFor="name">Votre nom :</label>
-                <input type="text" id="name" placeholder="veuillez saisir votre nom complet" { ...register("name", { required: true }) } required/>
+                <input disabled={true} type="text" id="name" placeholder="veuillez saisir votre nom complet" { ...register("name", { required: true }) } required/>
             </div>
             <div className="form-element">
                 <label htmlFor="email">Votre email :</label>
-                <input type="email" id="email" placeholder="veuillez saisir votre adresse email" { ...register("email", { required: true }) } required/>
+                <input disabled={true} type="email" id="email" placeholder="veuillez saisir votre adresse email" { ...register("email", { required: true }) } required/>
             </div>
         </form>
     )
