ed68fc6d81407e2ca6a582cdd755c59a0c36a250
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-05 08:55:29 +0300

    développement du formulaire de changement de mot de passe

--- src/components/dashboard/settings/changePasswordForm.jsx
+++ src/components/dashboard/settings/changePasswordForm.jsx
@@ -1,3 +1,11 @@
 export default function ChangePasswordForm(){
-    return(<h1>This is the changing password form;)</h1>)
+    return(
+        <form>
+            <h2>Mot de passe :</h2>
+            <div className="form-element">
+                <label htmlFor="password">Votre mot de passe actuel :</label>
+                <input type="password" name="password" id="password" required placeholder="veuillez choisir un mot de passe fort" />
+            </div>
+        </form>
+    )
 }
