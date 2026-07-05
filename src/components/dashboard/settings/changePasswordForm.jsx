164ba798496e368e499ab5c3c96d70bbc442bde3
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-05 09:06:51 +0300

    développement encore du composant de changement de mot de passe

--- src/components/dashboard/settings/changePasswordForm.jsx
+++ src/components/dashboard/settings/changePasswordForm.jsx
@@ -1,10 +1,22 @@
+import { useEffect } from "react"
+import { useForm } from "react-hook-form"
+
 export default function ChangePasswordForm(){
+
+    const { reset, register } = useForm()
+
+    useEffect(()=>{
+        reset({
+            password: "****************************"
+        })
+    },[reset])
+
     return(
         <form>
             <h2>Mot de passe :</h2>
             <div className="form-element">
                 <label htmlFor="password">Votre mot de passe actuel :</label>
-                <input type="password" name="password" id="password" required placeholder="veuillez choisir un mot de passe fort" />
+                <input type="password" id="password" { ...register("password", {required:true}) } required placeholder="veuillez choisir un mot de passe fort" />
             </div>
         </form>
     )
