0d57859de89091bfc81f859cb84f361b2500e507
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-12 13:43:24 +0300

    ajout de commentaire anti-lint

--- src/components/dashboard/songs/songs-list.jsx
+++ src/components/dashboard/songs/songs-list.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/incompatible-library */
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable react-hooks/exhaustive-deps */
 import Image from "next/image"
