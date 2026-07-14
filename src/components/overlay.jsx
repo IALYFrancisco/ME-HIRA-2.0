cdc8ef414aabdf7f13ddc40f68af01ec299d91e0
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-14 19:03:57 +0300

    changement de classe active

--- src/components/overlay.jsx
+++ src/components/overlay.jsx
@@ -1,3 +1,3 @@
 export default function Overlay(props) {
-    return(<div className={ props.overlayState ? "overlay actif" : "overlay"}></div>)
+    return(<div className={ props.overlayState ? "overlay enabled" : "overlay"}></div>)
 }
