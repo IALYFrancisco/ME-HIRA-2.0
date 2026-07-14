ded9f29717f21feea9c529a2f03dbd1a9ee17dbd
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-14 18:58:11 +0300

    développement encore du overlay global

--- src/components/overlay.jsx
+++ src/components/overlay.jsx
@@ -1,3 +1,8 @@
+import { useState } from "react"
+
 export default function Overlay() {
+
+    const [ internOverlayState, setInternOverlayState ] = useState(false)
+
     return(<div className={  "overlay"}></div>)
 }
