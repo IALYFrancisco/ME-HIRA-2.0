b86ca6f74f6faa5fa45cda32ca1d0666b913d69d
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-15 08:50:19 +0300

    développement du composant modal de suppression de document artiste

--- src/components/dashboard/artists/removeArtistDocumentModal.jsx
+++ src/components/dashboard/artists/removeArtistDocumentModal.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable react/no-unescaped-entities */
+import Image from "next/image"
+
 export default function RemoveArtistDocumentModal(){
     return(
         <div ref={removeSongModalRef} className="remove-song-modal">
