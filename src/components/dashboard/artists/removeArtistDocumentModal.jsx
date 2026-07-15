b68dcac532337a490ce922ffd0c99450fe92534c
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-15 08:48:16 +0300

    ajout de contenu dans le composant modal de suppression de document artiste

--- src/components/dashboard/artists/removeArtistDocumentModal.jsx
+++ src/components/dashboard/artists/removeArtistDocumentModal.jsx
@@ -1,3 +1,29 @@
 export default function RemoveArtistDocumentModal(){
-    return()
+    return(
+        <div ref={removeSongModalRef} className="remove-song-modal">
+            <h3>Suppression d'un document artiste.</h3>
+            { documentToDoAction &&
+                <>
+                    <p>
+                        Êtes-vous sûr(e) de vouloir supprimer les documents artiste de
+                        <strong> {documentToDoAction.artistName} </strong>
+                        ?
+                    </p>
+                    <p>Faite attention, cette action est irréversible.</p>
+                </>
+            }
+            <div className="remove-song-choices">
+                <span onClick={handleClickNoButton}><button disabled={songActionIsLoading} className="no">Non</button></span>
+                <span>
+                    <button disabled={songActionIsLoading} onClick={()=>removeDocumentArtist(documentToDoAction)} className="yes">
+                        { 
+                            songActionIsLoading ?
+                            <Image src="/images/spinner.svg" priority alt="chargement recherche des chansons selon leur titre et chanteurs" width={48} height={48} className="loader-search-icone" />
+                            : "Oui"
+                        }
+                    </button>
+                </span>
+            </div>
+        </div>
+    )
 }
