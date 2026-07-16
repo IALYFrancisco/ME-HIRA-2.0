fea6d995b9a683b1f43253bd8e5fa4ee985d9785
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-16 10:54:46 +0300

    développement encore du composant des thèmes

--- src/components/dashboard/settings/ThemesManager.jsx
+++ src/components/dashboard/settings/ThemesManager.jsx
@@ -2,6 +2,7 @@ export default function ThemesManager(){
     return(
         <section className="themes-container">
             <h2>Gestionnaire des themes :</h2>
+            <p>Vous pouvez faire un choix parmi les thèmes suivants :</p>
         </section>
     )
 }
