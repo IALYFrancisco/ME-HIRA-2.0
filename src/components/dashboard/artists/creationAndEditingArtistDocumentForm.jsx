975261803d7a28c22ed2d7c669345ce3332a1942
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-16 06:42:43 +0300

    encore un autre prop dans le composant formulaire

--- src/components/dashboard/artists/creationAndEditingArtistDocumentForm.jsx
+++ src/components/dashboard/artists/creationAndEditingArtistDocumentForm.jsx
@@ -12,6 +12,7 @@ export default function CreationAndEditingArtistDocumentForm({
     handleFileChange,
     toggleContactPhoneNumber,
     contactPhoneNumberIsActif,
+    toggleContactEmail,
     contactEmailIsActif
 }){
 
