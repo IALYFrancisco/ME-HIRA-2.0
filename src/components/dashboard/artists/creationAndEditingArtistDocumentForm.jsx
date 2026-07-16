bfa978d2a1df3357e3ae3b0f46f976ec9c08291a
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-16 08:27:14 +0300

    suppression d'un prop sur le composant formulaire

--- src/components/dashboard/artists/creationAndEditingArtistDocumentForm.jsx
+++ src/components/dashboard/artists/creationAndEditingArtistDocumentForm.jsx
@@ -13,7 +13,6 @@ export default function CreationAndEditingArtistDocumentForm({
     localFileIsDefined,
     hostedFileIsDefined,
     handleFileChange,
-    toggleContactPhoneNumber,
     contactPhoneNumberIsActif,
     toggleContactEmail,
     contactEmailIsActif,
