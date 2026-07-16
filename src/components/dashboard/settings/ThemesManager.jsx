ac1982a696ed46c3b907aecfcba8da6482725b46
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-16 12:04:50 +0300

    développement encore du composant thèmes

--- src/components/dashboard/settings/ThemesManager.jsx
+++ src/components/dashboard/settings/ThemesManager.jsx
@@ -26,6 +26,9 @@ export default function ThemesManager(){
                     <p>Thème sombre</p>
                 </li>
             </ul>
+            <span className="border">
+                <button>Réinitialiser le thème</button>
+            </span>
         </section>
     )
 }
