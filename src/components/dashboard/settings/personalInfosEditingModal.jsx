3ac881115ef838c0e028f1fb16ac3277bfa41cbf
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-17 08:01:16 +0300

    développement du modal de changement d'informations

--- src/components/dashboard/settings/personalInfosEditingModal.jsx
+++ src/components/dashboard/settings/personalInfosEditingModal.jsx
@@ -2,6 +2,8 @@ export default function PersonalInfosEditingModal({personalInfosEditingModalStat
     return(
         <section className={ personalInfosEditingModalState ? "change-personal-infos-modal enabled" : "change-personal-infos-modal"}>
             <h3>Changement sur les informations personnelles :</h3>
+            <p>Afin de pouvoir modifier vos informations personnelles, vous devez vous identifier.</p>
+            <p>Si votre adresse email reçoit une modification, vous serez déconnecté de votre compte</p>
         </section>
     )
 }
