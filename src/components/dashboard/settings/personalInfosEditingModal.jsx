eb1efd58e3bfe194b1b32cb230c417c370af2570
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-17 06:56:41 +0300

    correction de bug dû à un sélécteur CSS

--- src/components/dashboard/settings/personalInfosEditingModal.jsx
+++ src/components/dashboard/settings/personalInfosEditingModal.jsx
@@ -1,3 +1,3 @@
-export default function PersonalInfosEditingModal(personalInfosEditingModalState){
+export default function PersonalInfosEditingModal({personalInfosEditingModalState}){
     return(<h1 className={ personalInfosEditingModalState ? "change-personal-infos-modal enabled" : "change-personal-infos-modal"}>This is a modal ;)</h1>)
 }
