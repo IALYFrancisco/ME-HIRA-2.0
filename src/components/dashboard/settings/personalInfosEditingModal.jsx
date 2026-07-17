e8518090f5789d6ce688fc63512ce3c62fce2fda
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-17 07:03:28 +0300

    développement encore du modal de changement d'informations personnelles

--- src/components/dashboard/settings/personalInfosEditingModal.jsx
+++ src/components/dashboard/settings/personalInfosEditingModal.jsx
@@ -1,7 +1,7 @@
 export default function PersonalInfosEditingModal({personalInfosEditingModalState}){
     return(
         <section className={ personalInfosEditingModalState ? "change-personal-infos-modal enabled" : "change-personal-infos-modal"}>
-
+            <h3>Changement sur les informations personnelles :</h3>
         </section>
     )
 }
