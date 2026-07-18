07a9d67f508bef606d9dd9dd8c3ad2cdf0881d8c
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-18 13:56:25 +0300

    création d'une nouvelle architecture en utilisant le hook useForm()

--- src/components/dashboard/settings/personalInfosEditingModal.jsx
+++ src/components/dashboard/settings/personalInfosEditingModal.jsx
@@ -17,6 +17,9 @@ export default function PersonalInfosEditingModal({
         reset: resetUpdate,
         watch: watchUpdate 
     } = useForm()
+
+    const {} = useForm()
+    
     const { user, setUser } = useAuth()
     const [ userCanChange, setUserCanChange ] = useState(false)
     const [ userCkeckIsLoading, setUserCheckIsLoading ] = useState(false)
