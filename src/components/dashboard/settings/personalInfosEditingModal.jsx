7f0cf6b6825a3bd0b446ba5f2dd47e2bc897267f
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-18 08:29:57 +0300

    toujours dans le développement du fonctionnalité de changement d'informations

--- src/components/dashboard/settings/personalInfosEditingModal.jsx
+++ src/components/dashboard/settings/personalInfosEditingModal.jsx
@@ -2,17 +2,17 @@ import { useForm } from "react-hook-form"
 
 export default function PersonalInfosEditingModal({personalInfosEditingModalState}){
 
-    const { register } = useForm()
+    const { register, handleSubmit } = useForm()
 
     return(
         <section className={ personalInfosEditingModalState ? "change-personal-infos-modal enabled" : "change-personal-infos-modal"}>
             <h3>Changement sur les informations personnelles :</h3>
             <p>Afin de pouvoir modifier vos informations personnelles, vous devez vous identifier.</p>
             <p>Si votre adresse email reçoit une modification, vous serez déconnecté de votre compte puis vous authentifier à nouveau pour vous connecter.</p>
-            <form>
+            <form onSubmit={handleSubmit()}>
                 <div className="form-element">
                     <label htmlFor="password">Votre mot de passe :</label>
-                    <input type="password" name="password" id="password" placeholder="veuillez saisir votre mot de passe" />
+                    <input type="password" id="password" { ...register("password", {required:true}) } placeholder="veuillez saisir votre mot de passe" required />
                 </div>
                 <div className="form-element">
                     <span className="border">
