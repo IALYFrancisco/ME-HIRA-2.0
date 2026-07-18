c68911feb03f74f6deaf695d1ac82f9cde3b192b
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-18 08:39:25 +0300

    toujours dans le développement du fonctionnalité de modification des informations

--- src/components/dashboard/settings/personalInfosEditingModal.jsx
+++ src/components/dashboard/settings/personalInfosEditingModal.jsx
@@ -1,15 +1,30 @@
+import { useAuth } from "@/contexts/AuthContext"
+import { api } from "@/helpers/api"
 import { useForm } from "react-hook-form"
 
 export default function PersonalInfosEditingModal({personalInfosEditingModalState}){
 
     const { register, handleSubmit } = useForm()
+    const { user } = useAuth()
+
+    const changeUserInfos = async (data) => {
+        try{
+            const user = new FormData()
+            user.append("_id", user._id)
+            user.append("password", data.password)
+            let checkResponse = await api.post("/user/check", { user }) 
+        }
+        catch{
+
+        }
+    }
 
     return(
         <section className={ personalInfosEditingModalState ? "change-personal-infos-modal enabled" : "change-personal-infos-modal"}>
             <h3>Changement sur les informations personnelles :</h3>
             <p>Afin de pouvoir modifier vos informations personnelles, vous devez vous identifier.</p>
             <p>Si votre adresse email reçoit une modification, vous serez déconnecté de votre compte puis vous authentifier à nouveau pour vous connecter.</p>
-            <form onSubmit={handleSubmit()}>
+            <form onSubmit={handleSubmit(changeUserInfos)}>
                 <div className="form-element">
                     <label htmlFor="password">Votre mot de passe :</label>
                     <input type="password" id="password" { ...register("password", {required:true}) } placeholder="veuillez saisir votre mot de passe" required />
