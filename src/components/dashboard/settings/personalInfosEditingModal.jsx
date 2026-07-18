ec6d27ff873df82873dd37f1210f5e3b2e1dafd7
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-18 14:08:51 +0300

    développement encore du fonctionnalité de modification des informations personnelles

--- src/components/dashboard/settings/personalInfosEditingModal.jsx
+++ src/components/dashboard/settings/personalInfosEditingModal.jsx
@@ -15,7 +15,7 @@ export default function PersonalInfosEditingModal({
         register: registerUpdate,
         handleSubmit: handleSubmitUpdate,
         reset: resetUpdate,
-        watch: watchUpdate 
+        formState: { isDirty: isDirtyUpdateForm }
     } = useForm()
 
     const {
@@ -149,7 +149,7 @@ export default function PersonalInfosEditingModal({
                             <button>Annuler</button>
                         </span>
                         <span className="border">
-                            <button disabled={userInfosIsModified}>
+                            <button disabled={isDirtyUpdateForm}>
                                 { 
                                     userUpdateIsLoading ?
                                     <Image src="/images/spinner.svg" priority alt="chargement recherche des chansons selon leur titre et chanteurs" width={48} height={48} className="loader-search-icone" />
