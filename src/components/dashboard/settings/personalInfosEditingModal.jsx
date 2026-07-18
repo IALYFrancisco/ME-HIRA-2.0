ae5ef099c0d359bb1bd007d9fe0441c057d1347d
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-18 09:49:00 +0300

    création de fonction de changement d'informations personnelles

--- src/components/dashboard/settings/personalInfosEditingModal.jsx
+++ src/components/dashboard/settings/personalInfosEditingModal.jsx
@@ -40,6 +40,10 @@ export default function PersonalInfosEditingModal({
         }
     }
 
+    const changeUserPersonalInfos = (data) => {
+        return
+    } 
+
     return(
         <section className={ personalInfosEditingModalState ? "change-personal-infos-modal enabled" : "change-personal-infos-modal"}>
             <h3>Changement sur les informations personnelles :</h3>
