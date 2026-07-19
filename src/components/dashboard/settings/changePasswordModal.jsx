0e6cafd139aa9959dbc6f49b247c7251870330e1
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-19 07:17:39 +0300

    correction de bug d'import

--- src/components/dashboard/settings/changePasswordModal.jsx
+++ src/components/dashboard/settings/changePasswordModal.jsx
@@ -3,7 +3,7 @@ import { api } from "@/helpers/api"
 import { formToJSON } from "axios"
 import { useState } from "react"
 import { useForm } from "react-hook-form"
-import { toast& } from "sonner"
+import { toast } from "sonner"
 
 export default function ChangePasswordModal({
     changePasswordModalState,
