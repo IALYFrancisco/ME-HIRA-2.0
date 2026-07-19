b0df1a195a5c885114f7abae3cd57ad994951bc8
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-19 06:40:39 +0300

    création de deux props au composant 'ChangePasswordModal'

--- src/components/dashboard/settings/changePasswordModal.jsx
+++ src/components/dashboard/settings/changePasswordModal.jsx
@@ -1,3 +1,6 @@
-export default function ChangePasswordModal(){
+export default function ChangePasswordModal({
+    changePasswordModalState,
+    handleCloseChangePasswordModal
+}){
     return(<h1>Modal de changement de mot de passe</h1>)
 }
