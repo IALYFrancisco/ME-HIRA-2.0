08d05a1b9fe8b6d2fa7e9cc8597bf70b66f83565
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-19 07:18:51 +0300

    développement encore du fonction 'checkuser'

--- src/components/dashboard/settings/changePasswordModal.jsx
+++ src/components/dashboard/settings/changePasswordModal.jsx
@@ -39,7 +39,9 @@ export default function ChangePasswordModal({
             }
             setUserCanChange(false)
         }
-        finally{}
+        finally{
+            setUserCheckIsLoading(false)
+        }
     }
 
     return(
