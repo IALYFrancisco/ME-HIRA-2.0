74b1af8a71551c41fc932fb574cb8b6152c8a996
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-19 06:49:49 +0300

    développement encore du modal de changement de mot de passe

--- src/components/dashboard/settings/changePasswordModal.jsx
+++ src/components/dashboard/settings/changePasswordModal.jsx
@@ -3,6 +3,10 @@ export default function ChangePasswordModal({
     handleCloseChangePasswordModal
 }){
     return(
-        <section className={ changePasswordModalState ? "change-password-modal enabled" : "change-password-modal"}></section>
+        <section className={ changePasswordModalState ? "change-password-modal enabled" : "change-password-modal"}>
+            <h3>Changement de mot de passe :</h3>
+            <p>Afin de pouvoir modifier votre mot de passe, vous devez vous identifier.</p>
+            <p>Si votre adresse email reçoit une modification, vous serez déconnecté de votre compte puis vous authentifier à nouveau pour vous connecter.</p>
+        </section>
     )
 }
