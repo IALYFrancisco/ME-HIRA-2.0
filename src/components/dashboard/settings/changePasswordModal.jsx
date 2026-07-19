76958bd042eb201eefb76b52d6633a1a4a870d74
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-19 06:57:07 +0300

    développement encore du fonctionnalité de changement de mot de passe

--- src/components/dashboard/settings/changePasswordModal.jsx
+++ src/components/dashboard/settings/changePasswordModal.jsx
@@ -12,6 +12,10 @@ export default function ChangePasswordModal({
             <h3>Changement de mot de passe :</h3>
             <p>Afin de pouvoir modifier votre mot de passe, vous devez vous identifier.</p>
             <p>Suite au changement de votre mot de passe, vous serez déconnecté de votre compte puis vous authentifier à nouveau pour vous connecter.</p>
+            {
+                !userCanChange &&
+                <form></form>
+            }
         </section>
     )
 }
