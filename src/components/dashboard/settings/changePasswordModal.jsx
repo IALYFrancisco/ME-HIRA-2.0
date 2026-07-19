4a5c512b965fc2becbcdbdf524495c9c77e1380c
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-19 06:51:12 +0300

    changement de message dans le modal de changement de mot de passe

--- src/components/dashboard/settings/changePasswordModal.jsx
+++ src/components/dashboard/settings/changePasswordModal.jsx
@@ -6,7 +6,7 @@ export default function ChangePasswordModal({
         <section className={ changePasswordModalState ? "change-password-modal enabled" : "change-password-modal"}>
             <h3>Changement de mot de passe :</h3>
             <p>Afin de pouvoir modifier votre mot de passe, vous devez vous identifier.</p>
-            <p>Si votre adresse email reçoit une modification, vous serez déconnecté de votre compte puis vous authentifier à nouveau pour vous connecter.</p>
+            <p>Suite au changement de votre mot de passe, vous serez déconnecté de votre compte puis vous authentifier à nouveau pour vous connecter.</p>
         </section>
     )
 }
