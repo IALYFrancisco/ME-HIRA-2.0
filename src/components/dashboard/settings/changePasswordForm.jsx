858bd453b4b745a7435b44ea947a2b2edc0004c0
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-19 06:26:15 +0300

    développement encore du fonctionnalité de changement de mot de passe

--- src/components/dashboard/settings/changePasswordForm.jsx
+++ src/components/dashboard/settings/changePasswordForm.jsx
@@ -3,7 +3,7 @@ import { useForm } from "react-hook-form"
 
 export default function ChangePasswordForm(){
 
-    const { reset, register } = useForm()
+    const { reset, register, handleSubmit } = useForm()
     const [ overlayState, setOverlayState ] = useState(false)
     const [ changePasswordModalState, setChangePasswordModalState ] = useState(false)
 
@@ -13,8 +13,12 @@ export default function ChangePasswordForm(){
         })
     },[reset])
 
+    const handleSubmitForm = ()=>{
+        return
+    }
+
     return(
-        <form>
+        <form onSubmit={handleSubmit(handleSubmitForm)}>
             <h2>Mot de passe :</h2>
             <div className="form-element">
                 <label htmlFor="password">Votre mot de passe actuel :</label>
