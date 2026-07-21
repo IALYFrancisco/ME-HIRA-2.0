1b027adb50809c460e4515809c8da3e4aa127975
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-21 09:30:29 +0300

    développement encore du modal de réinitialisation de thème

--- src/components/dashboard/settings/ResetThemeModal.jsx
+++ src/components/dashboard/settings/ResetThemeModal.jsx
@@ -4,6 +4,7 @@ export default function ResetThemeModal({
     return(
         <div className={ resetThemeModalState ? "reset-theme-modal enabled" : "reset-theme-modal"}>
             <h3>Réinitialisation de thème :</h3>
+            <p>Voulez-vous réinitialiser le thème de votre espace ? <br/> Si oui, le thème sera défini sur le thème par défaut ( thème claire ).</p>
         </div>
     )
 }
