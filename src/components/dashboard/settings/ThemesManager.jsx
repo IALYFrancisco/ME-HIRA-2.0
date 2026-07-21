5e1c3d67021783cf1d30038dab0b3ffe57235e57
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-21 09:07:28 +0300

    intégration du composant 'Overlay' dans le composant 'ThemeManager'

--- src/components/dashboard/settings/ThemesManager.jsx
+++ src/components/dashboard/settings/ThemesManager.jsx
@@ -1,38 +1,41 @@
 import Image from "next/image"
-
+import Overlay from "@/components/overlay"
 
 export default function ThemesManager(){
     return(
-        <section className="themes-container">
-            <h2>Gestionnaire des themes :</h2>
-            <p>Vous pouvez faire un choix parmi les thèmes suivants :</p>
-            <ul className="themes-elements">
-                <li className="theme">
-                    <div>
+        <>
+            <section className="themes-container">
+                <h2>Gestionnaire des themes :</h2>
+                <p>Vous pouvez faire un choix parmi les thèmes suivants :</p>
+                <ul className="themes-elements">
+                    <li className="theme">
                         <div>
-                            <div className="light-theme"></div>
-                            <div className="dark-theme"></div>
+                            <div>
+                                <div className="light-theme"></div>
+                                <div className="dark-theme"></div>
+                            </div>
                         </div>
-                    </div>
-                    <p>Thème système</p>
-                </li>
-                <li className="theme">
-                    <Image src="/images/check.png" width={24} height={24} alt="check for current theme" priority />
-                    <div>
-                        <div></div>
-                    </div>
-                    <p>Thème claire</p>
-                </li>
-                <li className="theme">
-                    <div>
-                        <div></div>
-                    </div>
-                    <p>Thème sombre</p>
-                </li>
-            </ul>
-            <span className="border">
-                <button>Réinitialiser le thème</button>
-            </span>
-        </section>
+                        <p>Thème système</p>
+                    </li>
+                    <li className="theme">
+                        <Image src="/images/check.png" width={24} height={24} alt="check for current theme" priority />
+                        <div>
+                            <div></div>
+                        </div>
+                        <p>Thème claire</p>
+                    </li>
+                    <li className="theme">
+                        <div>
+                            <div></div>
+                        </div>
+                        <p>Thème sombre</p>
+                    </li>
+                </ul>
+                <span className="border">
+                    <button>Réinitialiser le thème</button>
+                </span>
+            </section>
+            <Overlay/>
+        </>
     )
 }
