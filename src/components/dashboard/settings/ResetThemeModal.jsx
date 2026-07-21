745429b12c9e2c56b8aab2e81368057f7c4929e5
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-07-21 09:00:44 +0300

    ajout de titre pour le modal

--- src/components/dashboard/settings/ResetThemeModal.jsx
+++ src/components/dashboard/settings/ResetThemeModal.jsx
@@ -3,7 +3,7 @@ export default function ResetThemeModal({
 }){
     return(
         <div className={ resetThemeModalState ? "reset-theme-modal enabled" : "reset-theme-modal"}>
-            
+            <h3>Réinitialisation de thème :</h3>
         </div>
     )
 }
