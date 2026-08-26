34b685a413757c94cc7670c70cfad147ac9b42bb
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-08-26 01:04:04 -0500

    définition de svg pour le contrôle 'chevronDown'

--- src/components/media-player/PlayerIcon.jsx
+++ src/components/media-player/PlayerIcon.jsx
@@ -119,6 +119,13 @@ export default function PlayerIcon({
                 </svg>
             );
 
+        case "chevronDown":
+            return (
+                <svg {...commonProps}>
+                    <polyline points="6 9 12 15 18 9" />
+                </svg>
+            );
+
 
     }
 
