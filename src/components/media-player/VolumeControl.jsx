8e867f381320dc2b8dc4dc23293d7730718a216c
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-08-26 01:38:28 -0500

    définition de tout les props du composant contrôleur de volume

--- src/components/media-player/VolumeControl.jsx
+++ src/components/media-player/VolumeControl.jsx
@@ -1,1 +1,11 @@
 import PlayerIcon from "./PlayerIcon";
+import styles from "./MediaPlayer.module.css"
+
+export default function VolumeControl({
+    volume,
+    muted,
+    noVolumeChange,
+    noMute
+}){
+
+}
