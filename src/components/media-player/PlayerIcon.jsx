82380f319254dd35df79caa885ed023ef8338298
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-08-26 01:03:01 -0500

    définition de svg pour le contrôle 'close'

--- src/components/media-player/PlayerIcon.jsx
+++ src/components/media-player/PlayerIcon.jsx
@@ -104,6 +104,14 @@ export default function PlayerIcon({
                 </svg>
             );
 
+        case "close":
+            return (
+                <svg {...commonProps}>
+                    <line x1="6" y1="6" x2="18" y2="18" />
+                    <line x1="18" y1="6" x2="6" y2="18" />
+                </svg>
+            );
+
 
     }
 
