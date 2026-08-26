f5686ffdb3516ed1cf061d7bf7a25b97dcde1316
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-08-26 06:09:15 -0500

    définition des états locaux nécéssaire au composant MediaPlayer

--- src/components/media-player/MediaPlayer.jsx
+++ src/components/media-player/MediaPlayer.jsx
@@ -3,6 +3,7 @@ import VolumeControl from "./VolumeControl";
 import SettingsMenu from "./SettingsMenu";
 
 import styles from "./MediaPlayer.module.css"
+import { useRef, useState } from "react";
 
 export default function MediaPlayer({
     song,
@@ -12,5 +13,32 @@ export default function MediaPlayer({
     poster,
     className = ""
 }){
+
+    const mediaRef = useRef(null)
+    const playerRef = useRef(null)
+
+    const hideControlsTimerRef = useRef(null)
+
+    const [ playing, setPlaying ] = useState(false)
+
+    const [ currentTime, setCurrentTime ] = useState(0)
+
+    const [ duration, setDuration ] = useState(0)
+
+    const [ volume, setVolume ] = useState(1)
+
+    const [ muted, setMuted ] = useState(false)
+
+    const [ playbackRate, setPlaybackRate ] = useState(1)
+
+    const [ settingsOpen, setSettingsOpen ] = useState(false)
+
+    const [ fullscreen, setFullscreen ] = useState(false)
+
+    const [ controlsVisible, setControlsVisible ] = useState(true)
+
+    const [ videoRatio, setVideoRatio ] = useState(null)
+
+    const [ loading, setLoading ] = useState(false)
 
 }
