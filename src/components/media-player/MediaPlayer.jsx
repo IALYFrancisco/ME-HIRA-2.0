c84527fb7168e2342e45d5e7d16725ae61bf1bed
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-08-26 03:38:44 -0500

    développement du composant de lecture personnalisé

---
+++ src/components/media-player/MediaPlayer.jsx
@@ -0,0 +1,9 @@
+import PlayerIcon from "./PlayerIcon";
+import VolumeControl from "./VolumeControl";
+import SettingsMenu from "./SettingsMenu";
+
+import styles from "./MediaPlayer.module.css"
+
+export default function MediaPlayer(){
+    
+}
