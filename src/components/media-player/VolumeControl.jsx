32ea162fb5d675bb699d69ff39e1e8e5e26cbdea
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-08-27 16:56:12 +0300

    correction de bug sur le gestionnaire de volume

--- src/components/media-player/VolumeControl.jsx
+++ src/components/media-player/VolumeControl.jsx
@@ -36,7 +36,7 @@ export default function VolumeControl({
                     value={ muted ? 0 : volume }
                     onChange={handleVolumeChange}
                     className={styles.volumeSlider}
-                    styles={{ "--volume": `${(muted ? 0 : volume) * 100}%` }}
+                    style={{ "--volume": `${(muted ? 0 : volume) * 100}%` }}
                     aria-label="volume"
                 />
             </div>
