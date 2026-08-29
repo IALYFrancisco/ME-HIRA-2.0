1414a12d4d096612200e330bb20640d1ada3b585
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-08-29 08:48:15 +0300

    intégration du composant 'Footer' dans la page des conditions générales d'utilisation

--- src/pages/terms-and-conditions.jsx
+++ src/pages/terms-and-conditions.jsx
@@ -1,9 +1,13 @@
 import Head from "next/head"
+import Footer from "@/components/footer";
 
 export default function TermsAndConditions(){
     return(
-        <Head>
-            <title>Conditions générales d'utilisation - Me-Hira</title>
-        </Head>
+        <>
+            <Head>
+                <title>Conditions générales d'utilisation - Me-Hira</title>
+            </Head>
+            <Footer/>
+        </>
     )
 }
