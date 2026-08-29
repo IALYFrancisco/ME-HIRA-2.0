8bcdf6564f4cc7fa19656d62340c425dc1518fd2
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-08-29 08:45:48 +0300

    petit changement sur la valeur du balise 'title'

--- src/pages/terms-and-conditions.jsx
+++ src/pages/terms-and-conditions.jsx
@@ -3,7 +3,7 @@ import Head from "next/head"
 export default function TermsAndConditions(){
     return(
         <Head>
-            <title>Conditions générales d'utilisation | Me-Hira</title>
+            <title>Conditions générales d'utilisation - Me-Hira</title>
         </Head>
     )
 }
