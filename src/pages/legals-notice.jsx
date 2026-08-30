7362a5a53fb2dfb285adb934497f4876b9045b1d
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-08-30 09:44:24 +0300

    séction 'Identification de la plateforme'

--- src/pages/legals-notice.jsx
+++ src/pages/legals-notice.jsx
@@ -1,6 +1,7 @@
 import Head from "next/head"
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
+import Link from "next/link"
 
 export default function LegalsNotice(){
     return(
@@ -12,6 +13,25 @@ export default function LegalsNotice(){
             <section className="legals-notice-container">
                 <h1>Mentions légales</h1>
                 <h3 className="update-date">Dernière mis à jour : 29 août 2026</h3>
+                <ul>
+                    <li className="terms-and-conditions-item">
+                        <h2>1. Identification de la plateforme</h2>
+                        <ul className="terms-and-conditions-subitem-container">
+                            <li>
+                                <p><strong>Nom :</strong> Me-Hira</p>
+                            </li>
+                            <li>
+                                <p><strong>Adresse actuellement utilisée :</strong> <Link href="/">https://mehira.onrender.com</Link></p>
+                            </li>
+                            <li>
+                                <p>Me-Hira est une plateforme numérique consacrée à la préservation, à la conservation et à la consultation d’œuvres musicales malagasy.</p>
+                            </li>
+                            <li>
+                                <p>La plateforme a notamment pour objectif de contribuer à la conservation numérique de chansons et d’œuvres musicales devenues difficiles ou impossibles à retrouver en ligne.</p>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
             </section>
             <Footer/>
         </>
