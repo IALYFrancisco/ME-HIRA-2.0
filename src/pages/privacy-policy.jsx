5a18c4b8c99bc2d2a4bee002a009bcad8848927c
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-08-30 07:30:37 +0300

    développement encore de la page des politiques de confidentialité

--- src/pages/privacy-policy.jsx
+++ src/pages/privacy-policy.jsx
@@ -1,3 +1,14 @@
+import Head from "next/head"
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
+
 export default function PrivacyPolicy() {
-    return(<></>)
+    return(
+        <>
+            <Head>
+                <title>Politique de confidentialité - Me-Hira</title>
+            </Head>
+            <Navbar/>
+        </>
+    )
 }
