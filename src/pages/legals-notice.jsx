0391d6976bd4c08a293d6230b51ab6137607ee76
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-08-30 09:26:02 +0300

    ajout de composant 'Navbar'

--- src/pages/legals-notice.jsx
+++ src/pages/legals-notice.jsx
@@ -8,6 +8,7 @@ export default function LegalsNotice(){
             <Head>
                 <title>Mentions légales - Me-Hira</title>
             </Head>
+            <Navbar/>
         </>
     )
 }
