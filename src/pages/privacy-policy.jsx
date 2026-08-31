cec86bf1289ae26408f78df25cb6ab84a842946e
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-08-31 10:10:09 +0300

    ajout des balises SEO dynamiques dans le page de politique de confidentialité

--- src/pages/privacy-policy.jsx
+++ src/pages/privacy-policy.jsx
@@ -11,9 +11,12 @@ export default function PrivacyPolicy() {
             <Head>
                 <title>Politique de confidentialité - Me-Hira</title>
                 <link rel="canonical" href="https://mehira.onrender.com/privacy-policy" />
+                <meta name="description" content={pageSEODescription}/>
                 <meta property="og:title" content="Politique de confidentialité - Me-Hira" />
                 <meta property="og:url" content="https://mehira.onrender.com/privacy-policy" />
+                <meta property="og:description" content={pageSEODescription}/>
                 <meta name="twitter:title" content="Politique de confidentialité - Me-Hira" />
+                <meta name="twitter:description" content={pageSEODescription} />
             </Head>
             <Navbar/>
             <section className="privacy-policy-container">
