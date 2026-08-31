0786688b0caa7a3aefc060aef7d2fa14e9c6bcf0
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-08-31 07:09:47 +0300

    ajout de balise og:url avec une valeur dynamique

--- src/pages/song/[slug].jsx
+++ src/pages/song/[slug].jsx
@@ -63,6 +63,7 @@ export default function SongReader({ song: _song }){
                 <title>{`${song?.title} - ${JoinArrayItems(song?.singer)} - Me-Hira`}</title>
                 <link rel="canonical" href={`https://mehira.onrender.com/song/${song?.slug}`} />
                 <meta property="og:title" content={`${song?.title} - ${JoinArrayItems(song?.singer)} - Me-Hira`} />
+                <meta property="og:url" content={`https://mehira.onrender.com/song/${song?.slug}`} />
                 <meta name="twitter:title" content={`${song?.title} - ${JoinArrayItems(song?.singer)} - Me-Hira`} />
             </Head>
             <Navbar/>
