683daa7298e6e00b7e8cdda5f2cb20914b8f3066
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-08-31 08:56:21 +0300

    ajout des balises dynamiques pour les déscriptions SEO de la page

--- src/pages/song/[slug].jsx
+++ src/pages/song/[slug].jsx
@@ -64,9 +64,12 @@ export default function SongReader({ song: _song }){
             <Head>
                 <title>{`${song?.title} - ${JoinArrayItems(song?.singer)} - Me-Hira`}</title>
                 <link rel="canonical" href={`https://mehira.onrender.com/song/${song?.slug}`} />
+                <meta name="description" content={pageDescription}/>
                 <meta property="og:title" content={`${song?.title} - ${JoinArrayItems(song?.singer)} - Me-Hira`} />
                 <meta property="og:url" content={`https://mehira.onrender.com/song/${song?.slug}`} />
+                <meta property="og:description" content={pageDescription}/>
                 <meta name="twitter:title" content={`${song?.title} - ${JoinArrayItems(song?.singer)} - Me-Hira`} />
+                <meta name="twitter:description" content={pageDescription} />
             </Head>
             <Navbar/>
             { _loadersState && <SongReaderSkeletonLoader/>}
