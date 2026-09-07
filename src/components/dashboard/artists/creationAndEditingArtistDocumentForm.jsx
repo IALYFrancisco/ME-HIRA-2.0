320aad389aa0d5168448d1432131af274307a774
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-09-07 08:26:29 +0300

    développement encore du fonctionnalité de modification de document artiste

--- src/components/dashboard/artists/creationAndEditingArtistDocumentForm.jsx
+++ src/components/dashboard/artists/creationAndEditingArtistDocumentForm.jsx
@@ -239,10 +239,13 @@ export default function CreationAndEditingArtistDocumentForm({
                 clearedFieldsUIText.push(cl.UIText)
             })
             
+            let confirmResult
             if(clearedFieldsUIText.length > 0){
-                window.confirm(`Etes-vous sûre de vouloir laisser vide les champs de saisie suivantes : ${JoinArrayItems(clearedFieldsUIText)} ?`)
+                confirmResult = window.confirm(`Etes-vous sûre de vouloir laisser vide les champs de saisie suivantes : ${JoinArrayItems(clearedFieldsUIText)} ?`)
             }
     
+            if(confirmResult)
+
             let response = await api.patch('/artist/update', updateAristDocumentFormData)
             if(response.status === 200){
                 toast.info(`Le document artiste de ${documentToDoAction?.artistName} a été bien modifié.`)
