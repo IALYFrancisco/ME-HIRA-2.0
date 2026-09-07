20743c70d76108fbc20133e8e4e34419b349866c
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-09-07 16:58:16 +0300

    suppression de deux états locaux de gestion de valeur des images

--- src/components/dashboard/artists/creationAndEditingArtistDocumentForm.jsx
+++ src/components/dashboard/artists/creationAndEditingArtistDocumentForm.jsx
@@ -26,8 +26,6 @@ export default function CreationAndEditingArtistDocumentForm({
     const { register, handleSubmit, reset, watch, setValue, formState: { isDirty } } = useForm()
 
     const [localFile, setLocalFile] = useState(null)
-    const [hostedFileIsDefined, setHostedFileIsDefined] = useState(false)
-    const [localFileIsDefined, setLocalFileIsDefined] = useState(false)
     const [createArtistDocumentIsLoading, setCreateArtistDocumentIsLoading] = useState(false)
 
     const isModified = isDirty || localFile
