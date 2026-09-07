bffd82b422cada05f1ee6e0ae7c43b2b832b785c
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-09-07 08:47:13 +0300

    amélioration encore du logique de modification de document 'artist'

--- src/components/dashboard/artists/creationAndEditingArtistDocumentForm.jsx
+++ src/components/dashboard/artists/creationAndEditingArtistDocumentForm.jsx
@@ -250,20 +250,23 @@ export default function CreationAndEditingArtistDocumentForm({
             }
     
             if(confirmResult){
+                
                 clearedFields.filter((cl)=>{
                     updateAristDocumentFormData.append(cl.CodeText, '')
                 })
+                
+                let response = await api.patch('/artist/update', updateAristDocumentFormData)
+                if(response.status === 200){
+                    toast.info(`Le document artiste de ${documentToDoAction?.artistName} a été bien modifié.`)
+                    api.get('/artist/get')
+                        .then((response) => {
+                            setArtists(response.data)
+                        })
+                        .catch(()=>toast.error("Erreur de récupération de la nouvelle liste des documents artiste."))
+                }
+                
             }
 
-            let response = await api.patch('/artist/update', updateAristDocumentFormData)
-            if(response.status === 200){
-                toast.info(`Le document artiste de ${documentToDoAction?.artistName} a été bien modifié.`)
-                api.get('/artist/get')
-                    .then((response) => {
-                        setArtists(response.data)
-                    })
-                    .catch(()=>toast.error("Erreur de récupération de la nouvelle liste des documents artiste."))
-            }
         }catch{
             toast.error("Erreur de modification du document, veuillez réessayer plus tard.")
         }finally{
