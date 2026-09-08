5fa0a573b0b41306ac9cc341571198cc8c22148c
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-09-08 05:55:13 +0300

    ajout d'un commentaire anti-eslint

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/incompatible-library */
 /* eslint-disable react-hooks/exhaustive-deps */
 import { api } from "@/helpers/api"
 import { JoinArrayItems } from "@/helpers/song"
