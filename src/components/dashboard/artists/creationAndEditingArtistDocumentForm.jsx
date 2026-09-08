708400beef0a9f7c124e1004306a504dd19d2f02
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-09-08 12:28:17 +0300

    mise en commentaire de certaines lignes de code

--- src/components/dashboard/artists/creationAndEditingArtistDocumentForm.jsx
+++ src/components/dashboard/artists/creationAndEditingArtistDocumentForm.jsx
@@ -248,9 +248,9 @@ export default function CreationAndEditingArtistDocumentForm({
                 }
             }
             
-            if(JoinArrayItems(documentToDoAction.roles) !== data.roles){
-                updateAristDocumentFormData.append('roles', data.roles)
-            }
+            // if(JoinArrayItems(documentToDoAction.roles) !== data.roles){
+            //     updateAristDocumentFormData.append('roles', data.roles)
+            // }
 
             const localFileUrl = documentToDoAction.image ? documentToDoAction.image : ''
             let formatedLocalFileUrl = ''
